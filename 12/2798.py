607834a9b6a3a61a008c548dd630527738300303
Hoon94 <dleognsdl1@naver.com>
2020-04-15 15:08:58 +0900

    알고리즘 12단계

--- 12/2798.py
+++ 12/2798.py
@@ -1,11 +1,14 @@
 N, M = map(int, input().split())
 card = list(map(int,input().split(' ')))
 
-answer = 0
-for i in range(0, N-2):
-    for j in range(i+1,N-1):
-        for k in range(j+1,N):
-            if (card[i]+card[j]+card[k]) <= M and M - (card[i]+card[j]+card[k]) < M - answer:
-                answer = card[i]+card[j]+card[k]
-                
-print(answer)
+card.sort()
+result = 0
+
+for i in range(N - 1, 1, -1):
+    for j in range(i - 1, 0, -1):
+        for k in range(j - 1, -1, -1):
+            if card[i] + card[j] + card[k] <= M and result < card[i] + card[j] + card[k]:
+                result = card[i] + card[j] + card[k]
+                break
+
+print(result)
