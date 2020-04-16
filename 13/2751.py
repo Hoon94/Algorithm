be7bcd394640fc0009247acc2030ad6b27380aba
Hoon94 <dleognsdl1@naver.com>
2020-04-16 18:58:17 +0900

    알고리즘 13단계

--- 13/2751.py
+++ 13/2751.py
@@ -6,7 +6,10 @@ order = list()
 for i in range(N):
     order.append(int(sys.stdin.readline()))
 
-order.sort()
+#order.sort()
 
-for _ in order:
+for _ in sorted(order):
     sys.stdout.write(str(_) + '\n')
+
+#파이썬은 기본 정렬 함수가 O(nlog(n))으로 구성.
+#merge sort, heap sort를 사용하지 않아도 된다.
