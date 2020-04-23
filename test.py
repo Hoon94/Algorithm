941793c8ac3efd0ef1988455146f9484b8b526c5
Hoon94 <dleognsdl1@naver.com>
2020-04-23 14:48:48 +0900

    파이썬 collection counter 모듈

--- test.py
+++ test.py
@@ -1,23 +1,24 @@
-a = [1,2,3,4,5,6,7,8,9] 
-b = a 
-c = list(a) 
-d = a.copy() 
-#b[0] = 10
-c[0] = 9
-d[0] = 8
-''' 
-from copy import deepcopy 
-e = [[1,2],[3,4],[5,6]] 
-f = e.copy() 
-g = deepcopy(e) 
-e[0] = [9,8]
-''' 
-print(a) 
-print(b)
-print(c)
-print(d)
-'''
-print(e)
-print(g)
-print(f)
-'''
+import sys
+
+N = int(sys.stdin.readline())
+arr = []
+count = 0
+
+for i in range(N):
+    arr.append(int(sys.stdin.readline()))
+
+arr.sort()
+
+#3 최빈값
+from collections import Counter
+k=Counter(arr).most_common()
+print(k)
+
+if len(arr) > 1:  #만약 입력값이 하나면 , 그게 최빈값이 되므로 예외처리
+    if k[0][1] == k[1][1]:
+        print(k[1][0]) 
+    # 최빈값의 빈도수를 비교하여, 2개이상의 최빈값이 있으면 두번째로 작은것을 출력
+    else: 
+        print(k[0][0]) 
+else: 
+    print(arr[0])
