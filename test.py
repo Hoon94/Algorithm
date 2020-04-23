ca4deb9fc2fc0fbac87874ab809086b6c15b0e4b
Hoon94 <dleognsdl1@naver.com>
2020-04-24 00:22:34 +0900

    max 함수 test

--- test.py
+++ test.py
@@ -1,28 +1,5 @@
-import sys
+a = [-5, 4, -3, 1]
 
-N = int(sys.stdin.readline())
-num = list(map(int, sys.stdin.readline().split()))
-K = int(sys.stdin.readline())
-case = []
-
-for i in range(K):
-    x, y = sys.stdin.readline().split()
-    case.append((int(x), int(y)))
-
-case = list(set(case))
-case.sort(key= lambda x : (-x[0], -x[1]))
-print(case)
-
-for i in range(len(case)):
-
-    if case[i][0] > case[i][1]:
-        case1 = num[0:case[i][0]]
-        case1.sort()
-        num[0:case[i][0]] = case1
-
-    case2 = num[0:case[i][1]]
-    case2.sort(reverse=True)
-    num[0:case[i][1]] = case2
-
-for i in num:
-    print(i, end= ' ')
+print(max(a))
+print(min(a))
+print(max(max(a), abs(min(a))))
