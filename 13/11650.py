45a54b31085b3399628a109826cf4bc2efdd498b
Hoon94 <dleognsdl1@naver.com>
2020-04-23 15:51:22 +0900

    알고리즘 13단계

--- 13/11650.py
+++ 13/11650.py
@@ -4,7 +4,8 @@ point = []
 for i in range(N):
     point.append(list(map(int, input().split())))
 
-point.sort()
+point.sort(key=lambda x: (x[0], x[1]))
+# x는 x[0] 다음 x[1]이 된다.
 
 for i, j in point:
     print(i, j)
