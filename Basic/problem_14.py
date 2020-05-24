5a44e266945881a3ba7cf85d53c571c5e646c4bd
Hoon94 <dleognsdl1@naver.com>
2020-05-24 23:24:54 +0900

    Basic exercises (14)

---
+++ Basic/problem_14.py
@@ -0,0 +1,8 @@
+import convert_temp
+
+print(convert_temp.C2F(50))
+print(convert_temp.F2C(50))
+print(convert_temp.C2K(50))
+print(convert_temp.K2C(50))
+print(convert_temp.F2K(50))
+print(convert_temp.K2F(50))
