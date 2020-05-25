04ce3a497821319211ccc1ed50282a72efdf10d3
Hoon94 <dleognsdl1@naver.com>
2020-05-26 00:19:26 +0900

    알고리즘

--- dfs/14502.py
+++ dfs/14502.py
@@ -2,6 +2,10 @@ from copy import deepcopy
 import sys
 sys.setrecursionlimit(1000)
 
+N = M = 0
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
 def dfs(x, y, arr):
     arr[x][y] = 2
 
@@ -9,62 +13,31 @@ def dfs(x, y, arr):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if nx < 0 or nx >= N or ny < 0 or ny >= M:
-            continue
-        
-        if arr[nx][ny] == 0:
-            dfs(nx, ny, arr)
+        if 0 <= nx and nx < N and 0 <= ny and ny < M:
+            if arr[nx][ny] == 0:
+                dfs(nx, ny, arr)
 
-    return arr
+    #return arr
 
-
-N, M = map(int, input().split())
-
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
-
-arr = [[0] * M for _ in range(N)]
-
-for i in range(N):
-    arr[i] = list(map(int, input().split()))
-
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == 2:
-            arr = dfs(i, j, arr)
-
-result = 0
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == 0:
-            result += 1
-
-print(arr)
-print(result)
-
-'''
-i = 0
-j = 0
-while True:
-    wall = deepcopy(arr)
-    cnt = 0
-    result = 0
+def getSafeArea(arr):
+    safe = 0
     for i in range(N):
         for j in range(M):
-            if wall[i][j] == 0:
-                wall[i][j] = 1
-                if cnt == 0:
-                    a = i
-                    b = j 
-                cnt += 1
-            
-            if cnt == 3:
-                wall = dfs(0, 0, wall)
-                for k in range(N):
-                    for l in range(M):
-                        if wall[k][l] == 0:
-                            result += 1
-                break
+            if arr[i][j] == 0:
+                safe += 1
+    
+    return safe
 
-print(result)
-'''
+#def setWall(start, depth):
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    arr = [[0] * M for _ in range(N)]
+
+    for i in range(N):
+        arr[i] = list(map(int, input().split()))
+
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 2:
+                dfs(i, j, arr)
