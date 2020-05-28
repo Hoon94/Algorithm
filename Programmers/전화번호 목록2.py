b6d54960c973bb5bdb8968e7fc01473b15e9206e
Hoon94 <dleognsdl1@naver.com>
2020-05-28 23:57:26 +0900

    Hash

--- Programmers/전화번호 목록2.py
+++ Programmers/전화번호 목록2.py
@@ -12,4 +12,16 @@ def solution(phone_book):
 phone_book = ["119", "97674223", "1195524421"]
 #result false
 
+'''
+#Test case 2
+phone_book = ["123", "456", "789"]
+#result true
+'''
+
+'''
+#Test case 3
+phone_book = ["12", "123", "1235", "567", "88"]
+#result false
+'''
+
 print(solution(phone_book))
