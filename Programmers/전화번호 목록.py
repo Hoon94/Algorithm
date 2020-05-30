cfbb02984141e88e99dc28b107fb9d332a0c3801
Hoon94 <dleognsdl1@naver.com>
2020-05-30 15:32:05 +0900

    Sort

---
+++ Programmers/전화번호 목록.py
@@ -0,0 +1,38 @@
+def solution(phone_book):   
+    phone_book.sort()
+
+    for i in range(len(phone_book) - 1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            return False
+        
+    return True
+
+'''
+#Test case 1
+phone_book = ["119", "97674223", "1195524421"]
+#result false
+'''
+
+'''
+#Test case 2
+phone_book = ["123", "456", "789"]
+#result true
+'''
+
+'''
+#Test case 3
+phone_book = ["12", "123", "1235", "567", "88"]
+#result false
+'''
+
+'''
+#Test case 4
+phone_book = ["12", "112", "012"]
+#result True
+'''
+
+#Test case 5
+phone_book = ["123", "12", "13"]
+#result false
+
+print(solution(phone_book))
