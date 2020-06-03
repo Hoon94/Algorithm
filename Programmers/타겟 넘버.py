d18843500e78492a084b69a4927b96191f49e0c3
Hoon94 <dleognsdl1@naver.com>
2020-06-04 00:02:48 +0900

    DFS

--- Programmers/타겟 넘버.py
+++ Programmers/타겟 넘버.py
@@ -1,19 +1,12 @@
-def dfs(numbers, target):
+def solution(numbers, target):    
     if len(numbers) == 1 and target == numbers[0]:
         return 1
     elif len(numbers) == 1 and target == -numbers[0]:
         return 1
     elif len(numbers) == 1:
         return 0
-    
-    return dfs(numbers[1:], target - numbers[0]) + dfs(numbers[1:], target + numbers[0])
 
-def solution(numbers, target):
-    answer = 0
-    
-    answer = dfs(numbers[1:], target - numbers[0]) + dfs(numbers[1:], target + numbers[0])
-    
-    return answer
+    return solution(numbers[1:], target - numbers[0]) + solution(numbers[1:], target + numbers[0])
 
 if __name__ == "__main__":
     
@@ -21,10 +14,12 @@ if __name__ == "__main__":
     numbers = [1, 1, 1, 1, 1]
     target = 3
     #result 5
+
     '''
     #Test case 2
     numbers = [1, 1]
     target = 2
     #result 1
     '''
+    
     print(solution(numbers, target))
