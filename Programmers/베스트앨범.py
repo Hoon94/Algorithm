d777c05f766e64cfbef7aca6f276a4e67958692c
Hoon94 <dleognsdl1@naver.com>
2020-06-03 21:43:41 +0900

    Hash

--- Programmers/베스트앨범.py
+++ Programmers/베스트앨범.py
@@ -1,26 +1,26 @@
 def solution(genres, plays):
     answer = []
     d = {}
-    album = []
+    album = {}
     
     for i in range(len(genres)):
-        d[genres[i]] = d.get(genres[i], []) + [plays[i]]
+        d[genres[i]] = d.get(genres[i], []) + [[plays[i], i]]
+        album[genres[i]] = album.get(genres[i], 0) + plays[i]
         
     for i in d:
-        d[i].sort()
-        album.append([i, sum(d[i])])
+        d[i].sort(key= lambda x : (x[0], -x[1]))
     
-    album.sort(key=lambda x : x[1], reverse=True)
+    s_album = sorted(album.items(), key= lambda x : x[1], reverse=True)
+
+    print(d)
     
-    for i in album:
+    for i in s_album:
         for j in range(2):
             if len(d[i[0]]) == 0:
                 break
-            where = plays.index(d[i[0]][-1])
-            answer.append(where)
-            plays[where] = -1
+            answer.append(d[i[0]][-1][1])
             d[i[0]] = d[i[0]][:-1]
-    
+
     return answer
 
 if __name__ == "__main__":
