7778105bd198cc4dd6b3fffd6f54ba83a4789018
Hoon94 <dleognsdl1@naver.com>
2020-06-04 15:57:39 +0900

    DFS

--- Programmers/네트워크2.py
+++ Programmers/네트워크2.py
@@ -1,16 +1,10 @@
 def dfs(x, y, computers, n):
     computers[x][y] = 0
-    nx = [-1, 0, 1, 0]
-    ny = [0, 1, 0, -1]
+    computers[y][x] = 0
     
-    for i in range(4):
-        dx = x + nx[i]
-        dy = y + ny[i]
-        
-        if dx < 0 or dx >= n or dy < 0 or dy >= n:
-            continue
-        if computers[dx][dy]:
-            dfs(dx, dy, computers, n)
+    for i in range(n):
+        if computers[y][i]:
+            dfs(y, i, computers, n)
     
 def solution(n, computers):
     answer = 0
