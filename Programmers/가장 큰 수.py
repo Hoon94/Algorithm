8f40311cd0baaba2809a65a59296291730128cb5
Hoon94 <dleognsdl1@naver.com>
2020-06-06 01:38:16 +0900

    Sort

---
+++ Programmers/가장 큰 수.py
@@ -0,0 +1,23 @@
+def solution(numbers):
+    answer = ''
+    numbers = [str(x) for x in numbers]
+    numbers.sort(key= lambda x : (x * 4)[:4], reverse=True)
+    if numbers[0] == '0':
+        answer = numbers[0]
+    else:
+        answer = ''.join(numbers)
+    return answer
+
+if __name__ == "__main__":
+    
+    #Test case 1
+    numbers = [6, 10, 2]
+    #result 
+
+    '''
+    #Test case 2
+    numbers = [3, 30, 34, 5, 9]
+    #result 
+    '''
+
+    print(solution(numbers))
