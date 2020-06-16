e4310f0aeba841580c45fa69aab70c20648c13c1
Hoon94 <dleognsdl1@naver.com>
2020-06-16 23:52:30 +0900

    Greedy

--- Programmers/조이스틱.py
+++ Programmers/조이스틱.py
@@ -1,39 +1,40 @@
 def solution(name):
-    answer = 0
-    name=list(name)
-    index=0
+    count = 0
+    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    d = {}
+    indexes = []
+    current_idx = 0
+    n = len(name)
 
-    while(True):
-        right=1
-        left=1
+    for i in range(len(alpha)):
+        d[alpha[i]] = min(i, 26 - i)
     
-        if name[index] != 'A': 
-            updown = min(ord(name[index])-ord('A'),(ord('Z')-ord(name[index])+1))
-            answer += updown
-        name[index] = 'A'
-    
-        if name == ["A"]*len(name):
+    for i in range(n):
+        num = d[name[i]]
+        count += num
+
+        if num != 0:
+            indexes.append(i)
+
+    while True:
+        if len(indexes) == 0:
             break
-    
-        for i in range(1,len(name)):
-            if name[index+i]=="A":
-                right+=1
-            else:
-                break
-    
-        for i in range(1,len(name)):
-            if name[index-i]=="A":
-                left+=1
-            else:
-                break
 
-        if right>left:
-            answer+=left
-            index-=left
-        else:
-            answer+=right
-            index+=right
-    return answer
+        min_dist = 99
+        min_idx = 0
+
+        for it in indexes:
+            min_dist2 = min(abs(it - current_idx), n - abs(it - current_idx))
+
+            if min_dist2 < min_dist:
+                min_dist = min_dist2
+                min_idx = it
+
+        count += min_dist
+        indexes.remove(min_idx)
+        current_idx = min_idx
+
+    return count
 
 if __name__ == "__main__":
     
