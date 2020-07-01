c691e339460a05695c1be675393957ff7773e4f8
Hoon94 <dleognsdl1@naver.com>
2020-07-01 19:40:26 +0900

    완전탐색

--- Programmers/카펫.py
+++ Programmers/카펫.py
@@ -4,3 +4,24 @@ def solution(brown, red):
             length = red // index
             if (((index + 2) * (length + 2)) - (index * length)) == brown:
                 return [max(index + 2, length + 2), min(index + 2, length + 2)]
+
+if __name__ == "__main__":
+    
+    #Test case 1
+    brown = 10
+    yellow = 8
+    #result [4, 3]
+
+    '''
+    #Test case 2
+    brown = 8
+    yellow = 1
+    #result[3, 3]
+    
+    #Test case 3
+    brown = 24
+    yellow = 24
+    #result [8, 6]
+    '''
+
+    print(solution(brown, yellow))
