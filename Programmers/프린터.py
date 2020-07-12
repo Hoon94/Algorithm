35f8534a9ceceb5e88b5172bc597ba901fbe7eb0
Hoon94 <dleognsdl1@naver.com>
2020-07-12 21:51:08 +0900

    Stack

--- Programmers/프린터.py
+++ Programmers/프린터.py
@@ -21,3 +21,19 @@ def solution(priorities, location):
             if pos.pop(0):
                 return count
             m = max(priorities)
+
+if __name__ == "__main__":
+    
+    #Test case 1
+    priorities = [2, 1, 3, 2]
+    location = 2
+    #result 1
+
+    '''
+    #Test case 2
+    priorities = [1, 1, 9, 1, 1, 1]
+    location = 0
+    #result 5
+    '''
+
+    print(solution(priorities, location))
