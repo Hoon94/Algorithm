23de227b775f8475307559efe037705dda3ef9ca
Hoon94 <dleognsdl1@naver.com>
2020-07-14 23:34:32 +0900

    Linked List

--- test.py
+++ test.py
@@ -1,10 +1,39 @@
-import queue
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
 
-data = queue.PriorityQueue()
+def add(data):
+    node = head
+    while node.next:
+        node = node.next
+    node.next = Node(data) 
 
-data.put((10, 5))
-data.put((3, 10))
-data.put((9, 1))
+node1 = Node(1)
+head = node1
+for index in range(2, 10):
+    add(index)
 
-print(data.qsize())
-print(data.get())
+node = head
+while node.next:
+    #print(node.data)
+    node = node.next
+#print (node.data)
+
+node = head
+node3 = Node(1.5)
+search = True
+while search:
+    if node.data == 1:
+        search = False
+    else:
+        node = node.next
+
+node_next = node.next
+node.next = node3
+node3.next = node_next
+
+while node.next:
+    print(node.data)
+    node = node.next
+print (node.data)
