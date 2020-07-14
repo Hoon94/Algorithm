d51c43196609e6f18fd0b64e3c28974ac9645ac1
Hoon94 <dleognsdl1@naver.com>
2020-07-15 00:43:16 +0900

    Double Linked List

--- test.py
+++ test.py
@@ -28,8 +28,30 @@ class NodeMgmt:
             print (node.data)
             node = node.next
 
+    def search_from_head(self, data):
+        if self.head == None:
+            return False
+    
+        node = self.head
+        while node:
+            if node.data == data:
+                return node
+            else:
+                node = node.next
+        return False
+
+
 if __name__ == "__main__":
     double_linked_list = NodeMgmt(0)
     for data in range(1, 10):
         double_linked_list.insert(data)
-    double_linked_list.desc()
+    #double_linked_list.desc()
+
+    node_3 = double_linked_list.search_from_head(3)
+    print(node_3.data)
+
+    node_3 = double_linked_list.search_from_head(10)
+    if node_3:
+        print(node_3.data)
+    else:
+        print("No data")
