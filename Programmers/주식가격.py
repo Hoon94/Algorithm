d8ee8391b800cdc577d6f800b6bf27ce8c21e172
Hoon94 <dleognsdl1@naver.com>
2020-07-19 21:07:12 +0900

    Stack

--- Programmers/주식가격.py
+++ Programmers/주식가격.py
@@ -8,3 +8,11 @@ def solution(prices):
             else:
                 answer[i] +=1
     return answer
+
+if __name__ == "__main__":
+    
+    #Test case 1
+    prices = [1, 2, 3, 2, 3]
+    #result [4, 3, 1, 1, 0]
+
+    print(solution(prices))
