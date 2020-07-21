66f5e6f301ec602bd03fbb44a8ff0539d4904309
Hoon94 <dleognsdl1@naver.com>
2020-07-21 23:19:26 +0900

    Dynamic Programming

--- Study/DP/JLIS.py
+++ Study/DP/JLIS.py
@@ -1,20 +1,20 @@
 #cache = [[-1] * 101] * 101
-'''
+
 A = [1, 2, 3]
 B = [4, 5, 6]
 '''
 A = [10, 20, 30, 1, 2]
 B = [10, 20, 30]
+'''
 
-n = 5 #len(A)
-m = 3 #len(B)
+n = len(A)
+m = len(B)
 cache = [[-1 for col in range(m + 1)] for row in range(n + 1)]
 
 def jlis(indexA : int, indexB : int):
-    print(indexA, indexB)
     if cache[indexA + 1][indexB + 1] != -1:
         return cache[indexA + 1][indexB + 1]
-    
+
     cache[indexA + 1][indexB + 1] = 2
     
     if indexA == -1:
@@ -28,6 +28,7 @@ def jlis(indexA : int, indexB : int):
         b = B[indexB]
     
     maxElement = max(a, b)
+    #print("indexA : {} indexB : {}, maxElement : {}".format(indexA, indexB, maxElement))
     
     nextA = indexA + 1
     while nextA < n:
@@ -35,15 +36,16 @@ def jlis(indexA : int, indexB : int):
             cache[indexA + 1][indexB + 1] = max(cache[indexA + 1][indexB + 1], jlis(nextA, indexB) + 1)
             print('nextA : ', nextA, 'indexB : ', indexB, 'value : ', cache[indexA + 1][indexB + 1])
         nextA += 1
-    
+
     nextB = indexB + 1
     while nextB < m:
         if maxElement < B[nextB]:
+            #print("indexA : {} indexB : {}, maxElement : {} nextB : {}".format(indexA, indexB, maxElement, nextB))
             cache[indexA + 1][indexB + 1] = max(cache[indexA + 1][indexB + 1], jlis(indexA, nextB) + 1)
             print('indexA : ', indexA, 'nextB : ', nextB, 'value : ', cache[indexA + 1][indexB + 1])
         nextB += 1
         
     return cache[indexA + 1][indexB + 1]
 
-print(cache)
 print(jlis(-1, -1))
+print(cache)
