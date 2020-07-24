cf57a146cdaee85b38a71f482b10c5833ae97fd1
Hoon94 <dleognsdl1@naver.com>
2020-07-25 01:52:09 +0900

    Dynamic Programming

---
+++ Study/DP/Quantization.py
@@ -0,0 +1,54 @@
+pSum = [0] * 101
+pSqSum = [0] * 101
+cache= [[-1 for x in range(11)] for y in range(11)]
+n = 10
+s = 3
+num = [3, 3, 3, 1, 2, 3, 2, 2, 2, 1]
+
+def saveSum():
+    global num
+    num = sorted(num)
+
+    pSum[0] = num[0]
+    pSqSum[0] = num[0] ** 2
+    for i in range(1, n):
+        pSum[i] = pSum[i - 1] + num[i]
+        pSqSum[i] = pSqSum[i - 1] + (num[i] ** 2)
+
+def minError(low : int, high : int):
+    if low == 0:
+        Sum = pSum[high]
+        sqSum = pSqSum[high]
+    else:
+        Sum = pSum[high] - pSum[low - 1]
+        sqSum = pSqSum[high] - pSqSum[low - 1]
+
+    m = round(Sum / (high - low + 1))
+    ret = sqSum - (2 * m * Sum) + (m * m * (high - low + 1))
+
+    return ret
+
+def quantize(start : int, parts : int):
+    if start == n:
+        return 0
+
+    if parts == 0:
+        return 10000
+    
+    if cache[start][parts] != -1:
+        return cache[start][parts]
+    
+    cache[start][parts] = 10000
+
+    for partsize in range(1, (n - start + 1)):
+        cache[start][parts] = min(cache[start][parts], minError(start, start + partsize - 1) + quantize(start + partsize, parts - 1))
+
+    return cache[start][parts]
+
+if __name__ == "__main__":
+    saveSum()
+    print(num)
+    print(quantize(0, s))
+    
+    for c in cache:
+        print(c)
