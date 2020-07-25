cfe3dcafd4db9a70653406f1ba276d16954eecce
Hoon94 <dleognsdl1@naver.com>
2020-07-26 00:09:31 +0900

    Binary Search Tree

--- test.py
+++ test.py
@@ -1,99 +1,47 @@
 class Node:
-    def __init__(self, data, prev=None, next=None):
-        self.prev = prev
-        self.data = data
-        self.next = next
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
 class NodeMgmt:
-    def __init__(self, data):
-        self.head = Node(data)
-        self.tail = self.head
-
-    def insert(self, data):
-        if self.head == None:
-            self.head = Node(data)
-            self.tail = self.head
-        else:
-            node = self.head
-            while node.next:
-                node = node.next
-            new = Node(data)
-            node.next = new
-            new.prev = node
-            self.tail = new
-
-    def desc(self):
-        node = self.head
-        while node:
-            print (node.data)
-            node = node.next
-
-    def search_from_head(self, data):
-        if self.head == None:
-            return False
+    def __init__(self, head):
+        self.head = head
     
-        node = self.head
-        while node:
-            if node.data == data:
-                return node
+    def insert(self, value):
+        self.current_node = self.head
+        while True:
+            if value < self.current_node.value:
+                if self.current_node.left != None:
+                    self.current_node = self.current_node.left
+                else:
+                    self.current_node.left = Node(value)
+                    break
             else:
-                node = node.next
+                if self.current_node.right != None:
+                    self.current_node = self.current_node.right
+                else:
+                    self.current_node.right = Node(value)
+                    break
+    
+    def search(self, value):
+        self.current_node = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                return True
+            elif value < self.current_node.value:
+                self.current_node = self.current_node.left
+            else:
+                self.current_node = self.current_node.right
         return False
 
-    def search_from_tail(self, data):
-        if self.head == None:
-            return False
-    
-        node = self.tail
-        while node:
-            if node.data == data:
-                return node
-            else:
-                node = node.prev
-        return False
 
-    
-    def insert_before(self, data, before_data):
-        if self.head == None:
-            self.head = Node(data)
-            return True
-        else:
-            node = self.tail
-            while node.data != before_data:
-                node = node.prev
-                if node == None:
-                    return False
-            new = Node(data)
-            before_new = node.prev
-            before_new.next = new
-            new.prev = before_new
-            new.next = node
-            node.prev = new
-            return True
+head = Node(1)
+BST = NodeMgmt(head)
+BST.insert(2)
+BST.insert(3)
+BST.insert(0)
+BST.insert(4)
+BST.insert(8)
 
-
-if __name__ == "__main__":
-    double_linked_list = NodeMgmt(0)
-    for data in range(1, 10):
-        double_linked_list.insert(data)
-    #double_linked_list.desc()
-
-    '''
-    #앞에서부터 검색
-    node_3 = double_linked_list.search_from_head(3)
-    print(node_3.data)
-
-    node_3 = double_linked_list.search_from_head(10)
-    if node_3:
-        print(node_3.data)
-    else:
-        print("No data")
-    
-    #뒤에서부터 검색
-    node_3 = double_linked_list.search_from_tail(3)
-    print(node_3.data)
-    '''
-
-    #data 값이 2 인 노드 앞에 1.5 값을 갖는 노드 생성
-    double_linked_list.insert_before(1.5, 2)
-    double_linked_list.desc()
+print(BST.search(-1))
