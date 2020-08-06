bd748dd8b1e6194fb5cf76590e11662f230af874
Hoon94 <dleognsdl1@naver.com>
2020-08-06 23:54:43 +0900

    테스트 타일링

--- Study/test.py
+++ Study/test.py
@@ -1,4 +1,14 @@
-c = int(input())
-print(c)
-print(type(c))
-# input() 함수는 항상 str type으로 입력 받는다.
+import sys
+
+num = int(sys.stdin.readline())
+
+arr = [0] * (num + 1)
+
+arr[1] = 1
+if num > 1:
+    arr[2] = 3
+
+for i in range(3, num + 1):
+    arr[i] = arr[i - 2] * 2 + arr[i - 1]
+
+sys.stdout.write(str(arr[num] % 10007))
