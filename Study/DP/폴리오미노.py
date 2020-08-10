8de347e66010141ebacd211dbfa82d8f3608ff7a
Hoon94 <dleognsdl1@naver.com>
2020-08-10 22:08:58 +0900

    Dynamic Programming

--- Study/DP/폴리오미노.py
+++ Study/DP/폴리오미노.py
@@ -1,21 +1,21 @@
-def poly(n, d):
-    if d == n:
+def poly(n : int, down : int):
+    if down == n:
         return 1
     
-    if board[n][d] != -1:
-        return board[n][d]
+    if cache[n][down] != -1:
+        return cache[n][down]
     
-    x = n - d
+    x = n - down
     ret = 0
 
     for o in range(1, x + 1):
-        ret += (o + d - 1) * poly(x, o)
+        ret += (o + down - 1) * poly(x, o)
         ret %= 10000000
-    board[n][d] = ret
+    cache[n][down] = ret
     return ret
  
 if __name__ == "__main__":
-    board = [[-1 for j in range(101)] for k in range(101)]
+    cache = [[-1 for j in range(101)] for k in range(101)]
     case = int(input())
     
     for i in range(case):
