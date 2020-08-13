6ea4eb0a2a682587a121bd5178251bef8f49c83a
Hoon94 <dleognsdl1@naver.com>
2020-08-13 23:09:18 +0900

    Dynamic Programming

--- Programmers/정수 삼각형.py
+++ Programmers/정수 삼각형.py
@@ -1,7 +1,7 @@
-def solution(tri):
+def solution(triangle):
     m_lst = []
 
-    for row in tri:
+    for row in triangle:
         lst = []
 
         if len(m_lst) == 0:
@@ -18,3 +18,11 @@ def solution(tri):
         m_lst.append(lst)
     
     return max(m_lst[-1])
+
+if __name__ == "__main__":
+    
+    #Test case 1
+    triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
+    #result 30
+
+    print(solution(triangle))
