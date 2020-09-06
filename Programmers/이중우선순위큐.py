943755ddc4fb78790db1d96f06a3243ce0d238ad
Hoon94 <dleognsdl1@naver.com>
2020-09-06 23:49:35 +0900

    Heap

--- Programmers/이중우선순위큐.py
+++ Programmers/이중우선순위큐.py
@@ -1,7 +1,6 @@
 import heapq
 
 def solution(operations):
-    answer = []
     heap = []
     
     for op in operations:
@@ -19,3 +18,17 @@ def solution(operations):
         return [0, 0]
     
     return [max(heap), min(heap)]
+
+if __name__ == "__main__":
+    
+    '''
+    #Test case 1
+    operations = ["I 16", "D 1"]
+    #result [0, 0]
+    '''
+
+    #Test case 2
+    operations = ["I 7", "I 5", "I -5", "D -1"]
+    #result [7, 5]
+
+    print(solution(operations))
