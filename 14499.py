726ebac656aacaf52c34c4cfdc7d9e8636a0ce79
Hoon94 <dleognsdl1@naver.com>
2020-10-21 21:57:21 +0900

    주사위 굴리기

--- 14499.py
+++ 14499.py
@@ -17,26 +17,27 @@ def dire(direction):
     elif direction == 3: return -1, 0
     elif direction == 4: return 1, 0
 
-n, m, x, y, k = map(int, input().split())
-s = []
+if __name__ == "__main__":    
+    n, m, x, y, k = map(int, input().split())
+    s = []
 
-for i in range(n):
-    s.append(list(map(int, input().split())))
+    for i in range(n):
+        s.append(list(map(int, input().split())))
 
-com = list(map(int, input().split()))
-dice = [0, 0, 0, 0, 0, 0, 0]
+    com = list(map(int, input().split()))
+    dice = [0, 0, 0, 0, 0, 0, 0]
 
-for i in com:
-    a, b = dire(i)
-    if 0 <= x + a < n and 0 <= y + b < m:
-        x += a
-        y += b
-        move(i)
+    for i in com:
+        a, b = dire(i)
+        if 0 <= x + a < n and 0 <= y + b < m:
+            x += a
+            y += b
+            move(i)
 
-        if s[x][y] != 0:
-            dice[1] = s[x][y]
-            s[x][y] = 0
-        else:
-            s[x][y] = dice[1]
-        
-        print(dice[6])
+            if s[x][y] != 0:
+                dice[1] = s[x][y]
+                s[x][y] = 0
+            else:
+                s[x][y] = dice[1]
+            
+            print(dice[6])
