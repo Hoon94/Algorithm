5af46ee1f144feb3fba6a1ed8153926ce2b17cc2
Hoon94 <dleognsdl1@naver.com>
2020-10-23 23:31:27 +0900

    카드 구매하기

--- 11052.py
+++ 11052.py
@@ -1,13 +1,14 @@
-n = int(input())
+if __name__ == "__main__":
+    n = int(input())
 
-d = [0] * (n + 1)
-p = [0] + list(map(int, input().split()))
+    d = [0] * (n + 1)
+    p = [0] + list(map(int, input().split()))
 
-d[1] = p[1]
+    d[1] = p[1]
 
-for i in range(2, n + 1):
-    for j in range(1, i + 1):
-        if d[i] < d[i - j] + p[j]:
-            d[i] = d[i - j] + p[j]
+    for i in range(2, n + 1):
+        for j in range(1, i + 1):
+            if d[i] < d[i - j] + p[j]:
+                d[i] = d[i - j] + p[j]
 
-print(d[n])
+    print(d[n])
