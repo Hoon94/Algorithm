5eddf83367ef8adf80dbc190e2a02b743e9c41c6
Hoon94 <dleognsdl1@naver.com>
2020-10-29 23:38:37 +0900

    연산자 끼워넣기

--- 14888.py
+++ 14888.py
@@ -1,27 +1,28 @@
-N = int(input())
-nums = list(map(int, input().split()))
-add, sub, mul, div = map(int, input().split())
+if __name__ == "__main__":
+    N = int(input())
+    nums = list(map(int, input().split()))
+    add, sub, mul, div = map(int, input().split())
 
-min_, max_ = 1e9, -1e9
+    min_, max_ = 1e9, -1e9
 
-def dfs(i, res, add, sub, mul, div):
-    global max_, min_
+    def dfs(i, res, add, sub, mul, div):
+        global max_, min_
 
-    if i == N:
-        max_ = max(res, max_)
-        min_ = min(res, min_)
-        return
+        if i == N:
+            max_ = max(res, max_)
+            min_ = min(res, min_)
+            return
 
-    else:
-        if add:
-            dfs(i + 1, res + nums[i], add - 1, sub, mul, div)
-        if sub:
-            dfs(i + 1, res - nums[i], add, sub - 1, mul, div)
-        if mul:
-            dfs(i + 1, res * nums[i], add, sub, mul - 1, div)
-        if div:
-            dfs(i + 1, int(res / nums[i]), add, sub, mul, div - 1)
+        else:
+            if add:
+                dfs(i + 1, res + nums[i], add - 1, sub, mul, div)
+            if sub:
+                dfs(i + 1, res - nums[i], add, sub - 1, mul, div)
+            if mul:
+                dfs(i + 1, res * nums[i], add, sub, mul - 1, div)
+            if div:
+                dfs(i + 1, int(res / nums[i]), add, sub, mul, div - 1)
 
-dfs(1, nums[0], add, sub, mul, div)
-print(max_)
-print(min_)
+    dfs(1, nums[0], add, sub, mul, div)
+    print(max_)
+    print(min_)
