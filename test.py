f37ba10fe11cef83778ad81b7cae2497ad2c7c77
Hoon94 <dleognsdl1@naver.com>
2020-11-08 23:56:30 +0900

    선택 정렬

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import random
 
-def insertion_sort(data):
-    for index in range(len(data) - 1):
-        for index2 in range(index + 1, 0, -1):
-            if data[index2] < data[index2 - 1]:
-                data[index2], data[index2 - 1] = data[index2 - 1], data[index2]
-            else:
-                break
+def selection_sort(data):
+    for stand in range(len(data) - 1):
+        lowest = stand
+        for index in range(stand + 1, len(data)):
+            if data[lowest] > data[index]:
+                lowest = index
+        data[lowest], data[stand] = data[stand], data[lowest]
     return data
 
 if __name__ == "__main__":
     data_list = random.sample(range(100), 10)
-    print (insertion_sort(data_list))
+    print (selection_sort(data_list))
