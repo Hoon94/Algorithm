ce2bcbd84e43677d41d36a11f595dcb87acca50b
Hoon94 <dleognsdl1@naver.com>
2020-11-09 02:06:39 +0900

    Quick Sort

--- test.py
+++ test.py
@@ -4,14 +4,10 @@ def qsort(data):
     if len(data) <= 1:
         return data
     
-    left, right = list(), list()
     pivot = data[0]
-    
-    for index in range(1, len(data)):
-        if pivot > data[index]:
-            left.append(data[index])
-        else:
-            right.append(data[index])
+
+    left = [ item for item in data[1:] if pivot > item ]
+    right = [ item for item in data[1:] if pivot <= item ]
     
     return qsort(left) + [pivot] + qsort(right)
 
