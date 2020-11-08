f745821e54f9c20ed8aa0daa534d02ee3f601cf9
Hoon94 <dleognsdl1@naver.com>
2020-11-09 02:04:56 +0900

    Quick Sort

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 import random
 
-def selection_sort(data):
-    for stand in range(len(data) - 1):
-        lowest = stand
-        for index in range(stand + 1, len(data)):
-            if data[lowest] > data[index]:
-                lowest = index
-        data[lowest], data[stand] = data[stand], data[lowest]
-    return data
+def qsort(data):
+    if len(data) <= 1:
+        return data
+    
+    left, right = list(), list()
+    pivot = data[0]
+    
+    for index in range(1, len(data)):
+        if pivot > data[index]:
+            left.append(data[index])
+        else:
+            right.append(data[index])
+    
+    return qsort(left) + [pivot] + qsort(right)
 
 if __name__ == "__main__":
     data_list = random.sample(range(100), 10)
-    print (selection_sort(data_list))
+    print (qsort(data_list))
