b8cd26039bce5bdc977a311badf11d3a4f990fea
Hoon94 <dleognsdl1@naver.com>
2020-11-10 23:57:58 +0900

    Merge Sort

--- test.py
+++ test.py
@@ -1,16 +1,38 @@
 import random
 
-def qsort(data):
+def merge(left, right):
+    merged = list()
+    left_point, right_point = 0, 0
+    
+    # case1 - left/right 둘다 있을때
+    while len(left) > left_point and len(right) > right_point:
+        if left[left_point] > right[right_point]:
+            merged.append(right[right_point])
+            right_point += 1
+        else:
+            merged.append(left[left_point])
+            left_point += 1
+
+    # case2 - left 데이터가 없을 때
+    while len(left) > left_point:
+        merged.append(left[left_point])
+        left_point += 1
+        
+    # case3 - right 데이터가 없을 때
+    while len(right) > right_point:
+        merged.append(right[right_point])
+        right_point += 1
+    
+    return merged
+
+def mergesplit(data):
     if len(data) <= 1:
         return data
-    
-    pivot = data[0]
-
-    left = [ item for item in data[1:] if pivot > item ]
-    right = [ item for item in data[1:] if pivot <= item ]
-    
-    return qsort(left) + [pivot] + qsort(right)
+    medium = int(len(data) / 2)
+    left = mergesplit(data[:medium])
+    right = mergesplit(data[medium:])
+    return merge(left, right)
 
 if __name__ == "__main__":
     data_list = random.sample(range(100), 10)
-    print (qsort(data_list))
+    print (mergesplit(data_list))
