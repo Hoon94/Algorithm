226d617af33584a51f5c37ebb125214dfb4dc41e
Hoon94 <dleognsdl1@naver.com>
2020-11-12 13:32:42 +0900

    동전 1

--- 2293.py
+++ 2293.py
@@ -1,11 +1,12 @@
-n, k = map(int, input().split())
-c = [int(input()) for i in range(n)]
-dp = [0 for i in range(k + 1)]
-dp[0] = 1
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    c = [int(input()) for i in range(n)]
+    dp = [0 for i in range(k + 1)]
+    dp[0] = 1
 
-for i in c:
-    for j in range(1, k + 1):
-        if j - i >= 0:
-            dp[j] += dp[j - i]
-            
-print(dp[k])
+    for i in c:
+        for j in range(1, k + 1):
+            if j - i >= 0:
+                dp[j] += dp[j - i]
+                
+    print(dp[k])
