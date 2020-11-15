8fff94a25e74ca4dc233f361addcce39c32d630b
Hoon94 <dleognsdl1@naver.com>
2020-11-15 22:38:23 +0900

    N-Queen

--- test.py
+++ test.py
@@ -1,38 +1,22 @@
-import random
+n = int(input())
+s = [0 for i in range(16)]
+result = 0
 
-def merge(left, right):
-    merged = list()
-    left_point, right_point = 0, 0
-    
-    # case1 - left/right 둘다 있을때
-    while len(left) > left_point and len(right) > right_point:
-        if left[left_point] > right[right_point]:
-            merged.append(right[right_point])
-            right_point += 1
-        else:
-            merged.append(left[left_point])
-            left_point += 1
+def isTrue(x):
+    for i in range(1, x):
+        if s[x] == s[i] or abs(s[x] - s[i]) == x - i:
+            return False
+    return True
 
-    # case2 - left 데이터가 없을 때
-    while len(left) > left_point:
-        merged.append(left[left_point])
-        left_point += 1
-        
-    # case3 - right 데이터가 없을 때
-    while len(right) > right_point:
-        merged.append(right[right_point])
-        right_point += 1
-    
-    return merged
-
-def mergesplit(data):
-    if len(data) <= 1:
-        return data
-    medium = int(len(data) / 2)
-    left = mergesplit(data[:medium])
-    right = mergesplit(data[medium:])
-    return merge(left, right)
-
-if __name__ == "__main__":
-    data_list = random.sample(range(100), 10)
-    print (mergesplit(data_list))
+def dfs(cnt):
+    global result
+    if cnt > n:
+        result += 1
+    else:
+        for i in range(1, n + 1):
+            s[cnt] = i
+            if isTrue(cnt):
+                dfs(cnt + 1)
+                
+dfs(1)
+print(result)
