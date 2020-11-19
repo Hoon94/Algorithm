ae40524e8abbb37a71d1f0191616f2a995557252
Hoon94 <dleognsdl1@naver.com>
2020-11-20 01:44:54 +0900

    팰린드롬수

--- 1259.py
+++ 1259.py
@@ -5,16 +5,10 @@ if __name__ == "__main__":
 
     while True:
         num = input().strip()
-        check = len(num) // 2
 
         if num == '0':
             break
-        elif check == 0:
+        elif num == num[::-1]:
             print("yes")
         else:
-            for i in range(check):
-                if num[i] != num[-1 - i]:
-                    print("no")
-                    break
-                elif i == check - 1:
-                    print("yes")
+            print("no")
