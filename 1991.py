9809e1214c4387d20c794059100d746d25ef947d
Hoon94 <dleognsdl1@naver.com>
2020-11-23 21:55:39 +0900

    트리 순회

--- 1991.py
+++ 1991.py
@@ -1,34 +1,36 @@
-n = int(input())
-s = [[0] * 3 for i in range(26)]
+if __name__ == "__main__":
 
-def preorder(a):
-    print(a, end="")
-    if s[ord(a) - 65][1] != ".":
-        preorder(s[ord(a) - 65][1])
-    if s[ord(a) - 65][2] != ".":
-        preorder(s[ord(a) - 65][2])
+    n = int(input())
+    s = [[0] * 3 for i in range(26)]
 
-def inorder(a):
-    if s[ord(a) - 65][1] != ".":
-        inorder(s[ord(a) - 65][1])
-    print(a, end="")
-    if s[ord(a) - 65][2] != ".":
-        inorder(s[ord(a) - 65][2])
+    def preorder(a):
+        print(a, end="")
+        if s[ord(a) - 65][1] != ".":
+            preorder(s[ord(a) - 65][1])
+        if s[ord(a) - 65][2] != ".":
+            preorder(s[ord(a) - 65][2])
 
-def postorder(a):
-    if s[ord(a) - 65][1] != ".":
-        postorder(s[ord(a) - 65][1])
-    if s[ord(a) - 65][2] != ".":
-        postorder(s[ord(a) - 65][2])
-    print(a, end="")
+    def inorder(a):
+        if s[ord(a) - 65][1] != ".":
+            inorder(s[ord(a) - 65][1])
+        print(a, end="")
+        if s[ord(a) - 65][2] != ".":
+            inorder(s[ord(a) - 65][2])
 
-for i in range(n): 
-    ro, le, ri = map(str, input().split())
-    ro_ = ord(ro) - 65
-    s[ro_][0], s[ro_][1], s[ro_][2] = ro, le, ri
-    
-preorder("A")
-print()
-inorder("A")
-print()
-postorder("A")
+    def postorder(a):
+        if s[ord(a) - 65][1] != ".":
+            postorder(s[ord(a) - 65][1])
+        if s[ord(a) - 65][2] != ".":
+            postorder(s[ord(a) - 65][2])
+        print(a, end="")
+
+    for i in range(n): 
+        ro, le, ri = map(str, input().split())
+        ro_ = ord(ro) - 65
+        s[ro_][0], s[ro_][1], s[ro_][2] = ro, le, ri
+        
+    preorder("A")
+    print()
+    inorder("A")
+    print()
+    postorder("A")
