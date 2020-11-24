1c94849d3435c69c4a897b31fd7ade4e9ee1a5e4
Hoon94 <dleognsdl1@naver.com>
2020-11-24 23:10:48 +0900

    Greedy

--- test.py
+++ test.py
@@ -1,22 +1,16 @@
-n = int(input())
-s = [0 for i in range(16)]
-result = 0
+def min_coin_count(value, coin_list):
+    total_coin_count = 0
+    details = list()
+    coin_list.sort(reverse=True)
+    
+    for coin in coin_list:
+        coin_num = value // coin
+        total_coin_count += coin_num
+        value -= coin_num * coin
+        details.append([coin, coin_num])
+    
+    return total_coin_count, details
 
-def isTrue(x):
-    for i in range(1, x):
-        if s[x] == s[i] or abs(s[x] - s[i]) == x - i:
-            return False
-    return True
-
-def dfs(cnt):
-    global result
-    if cnt > n:
-        result += 1
-    else:
-        for i in range(1, n + 1):
-            s[cnt] = i
-            if isTrue(cnt):
-                dfs(cnt + 1)
-                
-dfs(1)
-print(result)
+if __name__ == "__main__":
+    coin_list = [500, 100, 50, 1]
+    print(min_coin_count(4720, coin_list))
