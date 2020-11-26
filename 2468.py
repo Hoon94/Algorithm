1e6c10815b9b7654cc6302db16eef76610397b10
Hoon94 <dleognsdl1@naver.com>
2020-11-26 22:14:58 +0900

    안전 영역

--- 2468.py
+++ 2468.py
@@ -1,45 +1,47 @@
-n = int(input())
-s = []
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-max_cnt = 0
+if __name__ == "__main__":
 
-def bfs(j, k):
-    queue = [[j, k]]
-    
-    while queue:
-        a, b = queue[0][0], queue[0][1]
-        del queue[0]
-        for k in range(4):
-            x = a + dx[k]
-            y = b + dy[k]
-            if 0 <= x < n and 0 <= y < n and copy[x][y] == 0:
-                copy[x][y] = 1
-                queue.append([x, y])
+    n = int(input())
+    s = []
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, -1, 1]
+    max_cnt = 0
 
-for i in range(n):
-    a = list(map(int, input().split()))
-    s.append(a)
+    def bfs(j, k):
+        queue = [[j, k]]
+        
+        while queue:
+            a, b = queue[0][0], queue[0][1]
+            del queue[0]
+            for k in range(4):
+                x = a + dx[k]
+                y = b + dy[k]
+                if 0 <= x < n and 0 <= y < n and copy[x][y] == 0:
+                    copy[x][y] = 1
+                    queue.append([x, y])
 
-for i in range(0, 101):
-    copy = [[0] * n for i in range(n)]
-    cnt = 0
+    for i in range(n):
+        a = list(map(int, input().split()))
+        s.append(a)
 
-    for j in range(n):
-        for k in range(n):
-            if s[j][k] <= i:
-                copy[j][k] = 1
-    
-    for j in range(n):
-        for k in range(n):
-            if copy[j][k] == 0:
-                copy[j][k] = 1
-                bfs(j, k)
-                cnt += 1
-    
-    if cnt == 0:
-        break
-    
-    max_cnt = max(max_cnt, cnt)
+    for i in range(0, 101):
+        copy = [[0] * n for i in range(n)]
+        cnt = 0
 
-print(max_cnt)
+        for j in range(n):
+            for k in range(n):
+                if s[j][k] <= i:
+                    copy[j][k] = 1
+        
+        for j in range(n):
+            for k in range(n):
+                if copy[j][k] == 0:
+                    copy[j][k] = 1
+                    bfs(j, k)
+                    cnt += 1
+        
+        if cnt == 0:
+            break
+        
+        max_cnt = max(max_cnt, cnt)
+
+    print(max_cnt)
