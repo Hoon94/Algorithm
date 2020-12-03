844515ba36e7294f066554ba71deca128586852c
Hoon94 <dleognsdl1@naver.com>
2020-12-03 23:17:19 +0900

    이항 계수 2

--- 11051.py
+++ 11051.py
@@ -1,11 +1,13 @@
-n, k = map(int, input().split())
-cache = []
+if __name__ == "__main__":
 
-for i in range(n + 1):
-    cache.append([1] * (i + 1))
+    n, k = map(int, input().split())
+    cache = []
 
-for i in range(2, n + 1):
-    for j in range(1, i):
-        cache[i][j] = (cache[i - 1][j - 1] + cache[i - 1][j]) % 10007
+    for i in range(n + 1):
+        cache.append([1] * (i + 1))
 
-print(cache[n][k])
+    for i in range(2, n + 1):
+        for j in range(1, i):
+            cache[i][j] = (cache[i - 1][j - 1] + cache[i - 1][j]) % 10007
+
+    print(cache[n][k])
