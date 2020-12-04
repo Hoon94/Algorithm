dc223d4e15a62d6b551ffa30943f7773ac7eb40d
Hoon94 <dleognsdl1@naver.com>
2020-12-04 15:44:55 +0900

    동전 2

---
+++ 2294.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = sorted([int(input()) for _ in range(n)])
+
+arr = [10001] * (k + 1)
+arr[0] = 0
+
+for i in range(n):
+    for j in range(coins[i], k + 1):
+        arr[j] = min(arr[j], arr[j - coins[i]] + 1)
+
+arr[-1] = arr[-1] if arr[-1] != 10001 else -1
+print(arr[-1])
