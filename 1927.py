d163e3fdb994d3003b56b97db93a09ba27e66116
Hoon94 <dleognsdl1@naver.com>
2020-12-11 20:25:11 +0900

    최소 힙

--- 1927.py
+++ 1927.py
@@ -3,16 +3,15 @@ import heapq
 
 if __name__ == "__main__":
     
-    numbers = int(input())
     heap = []
 
-    for _ in range(numbers):
+    for _ in range(int(input())):
         num = int(sys.stdin.readline())
         
         if num != 0:
             heapq.heappush(heap, num)
         else:
-            try:
+            try: # if heap:
                 print(heapq.heappop(heap))
-            except:
+            except: # else:
                 print(0)
