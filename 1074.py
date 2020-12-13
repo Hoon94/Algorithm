bc8c4a08228ccaf47bb735004cb5bb32f471b4c9
Hoon94 <dleognsdl1@naver.com>
2020-12-13 23:25:16 +0900

    Z

--- 1074.py
+++ 1074.py
@@ -1,28 +1,30 @@
-n, r, c = map(int, input().split())
-num = 0
+if __name__ == "__main__":
 
-while n > 0:
-    temp = (2 ** n) // 2
+    n, r, c = map(int, input().split())
+    num = 0
 
-    if n > 1:
-        if temp > r and temp <= c:
-            num += temp ** 2
-            c -= temp
-        elif temp <= r and temp > c:
-            num += (temp ** 2) * 2
-            r -= temp
-        elif temp <= r and temp <= c:
-            num += (temp ** 2) * 3
-            r -= temp
-            c -= temp
-    elif n == 1:
-        if r == 0 and c == 1:
-            num += 1
-        elif r == 1 and c == 0:
-            num += 2
-        elif r == 1 and c == 1:
-            num += 3
+    while n > 0:
+        temp = (2 ** n) // 2
 
-    n -= 1
-    
-print(num)
+        if n > 1:
+            if temp > r and temp <= c:
+                num += temp ** 2
+                c -= temp
+            elif temp <= r and temp > c:
+                num += (temp ** 2) * 2
+                r -= temp
+            elif temp <= r and temp <= c:
+                num += (temp ** 2) * 3
+                r -= temp
+                c -= temp
+        elif n == 1:
+            if r == 0 and c == 1:
+                num += 1
+            elif r == 1 and c == 0:
+                num += 2
+            elif r == 1 and c == 1:
+                num += 3
+
+        n -= 1
+        
+    print(num)
