ab5089585a3fd19884a559f30555887086d7a739
Hoon94 <dleognsdl1@naver.com>
2020-12-16 23:55:30 +0900

    톱니바퀴

--- 14891.py
+++ 14891.py
@@ -1,64 +1,66 @@
 import sys
 input = sys.stdin.readline
 
-def rotation(n, d):
-    isRotate = [False for i in range(4)]
-    direction = [0 for i in range(4)]
-    isRotate[n] = True
-    direction[n] = d
-    tempn = n
-    tempd = d
+if __name__ == "__main__":
+    
+    def rotation(n, d):
+        isRotate = [False for i in range(4)]
+        direction = [0 for i in range(4)]
+        isRotate[n] = True
+        direction[n] = d
+        tempn = n
+        tempd = d
 
-    for i in range(n + 1, 4):
-        if s[tempn][2] != s[i][6]:
-            isRotate[i] = True
-            tempd = -tempd
-            direction[i] = tempd
-            tempn = i
-        else:
-            break
+        for i in range(n + 1, 4):
+            if s[tempn][2] != s[i][6]:
+                isRotate[i] = True
+                tempd = -tempd
+                direction[i] = tempd
+                tempn = i
+            else:
+                break
 
-    tempn = n
-    tempd = d
+        tempn = n
+        tempd = d
 
-    for i in range(n - 1, -1, -1):
-        if s[tempn][6] != s[i][2]:
-            isRotate[i] = True
-            tempd = -tempd
-            direction[i] = tempd
-            tempn = i
-        else:
-            break
+        for i in range(n - 1, -1, -1):
+            if s[tempn][6] != s[i][2]:
+                isRotate[i] = True
+                tempd = -tempd
+                direction[i] = tempd
+                tempn = i
+            else:
+                break
+
+        for i in range(4):
+            if isRotate[i]:
+                if direction[i] == 1:
+                    temp = s[i].pop()
+                    s[i] = [temp] + s[i]
+                else:
+                    temp = s[i][0]
+                    del s[i][0]
+                    s[i].append(temp)
+
+    s = []
+    r = []
 
     for i in range(4):
-        if isRotate[i]:
-            if direction[i] == 1:
-                temp = s[i].pop()
-                s[i] = [temp] + s[i]
-            else:
-                temp = s[i][0]
-                del s[i][0]
-                s[i].append(temp)
+        s.append(list(input().strip()))
 
-s = []
-r = []
+    k = int(input())
 
-for i in range(4):
-    s.append(list(input().strip()))
+    for i in range(k):
+        a, b = map(int, input().split())
+        r.append([a - 1, b])
 
-k = int(input())
+    for n, d in r:
+        rotation(n, d)
 
-for i in range(k):
-    a, b = map(int, input().split())
-    r.append([a - 1, b])
+    result = 0
 
-for n, d in r:
-    rotation(n, d)
+    for i in range(4):
+        if s[i][0] == "1":
+            result += 2 ** i
 
-result = 0
-
-for i in range(4):
-    if s[i][0] == "1":
-        result += 2 ** i
-
-print(result)
+    print(result)
