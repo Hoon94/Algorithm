f2b141b1801e7e191a94957597756e3f8a7e88a7
Hoon94 <dleognsdl1@naver.com>
2021-01-05 23:42:32 +0900

    최대 값 구하기

--- test.py
+++ test.py
@@ -1,16 +1,10 @@
-def min_coin_count(value, coin_list):
-    total_coin_count = 0
-    details = list()
-    coin_list.sort(reverse=True)
-    
-    for coin in coin_list:
-        coin_num = value // coin
-        total_coin_count += coin_num
-        value -= coin_num * coin
-        details.append([coin, coin_num])
-    
-    return total_coin_count, details
+import heapq
 
-if __name__ == "__main__":
-    coin_list = [500, 100, 50, 1]
-    print(min_coin_count(4720, coin_list))
+nums = [4, 1, 7, 3, 8, 5]
+heap = []
+
+for num in nums:
+  heapq.heappush(heap, (-num, num))  # (우선 순위, 값)
+
+while heap:
+  print(heapq.heappop(heap)[1])  # index 1
