4de61f8f019f240006b7f984c1777911536ad71f
Hoon94 <dleognsdl1@naver.com>
2021-01-25 21:21:38 +0900

    Roman to Integer

--- Leetcode/13 Roman to Integer.py
+++ Leetcode/13 Roman to Integer.py
@@ -1,13 +1,22 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        rules = {"M": 1000, "D": 500, "C": 100,
-                 "L": 50, "X": 10, "V": 5, "I": 1}
-        res = rules[s[len(s) - 1]]
+    rules = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
 
-        for i in range(len(s) - 2, -1, -1):
-            if rules[s[i]] < rules[s[i + 1]]:
-                res -= rules[s[i]]
+    def romanToInt(self, s: str) -> int:
+        """[summary]
+
+        Args:
+            s (str): Roman number
+
+        Returns:
+            int: change Roman number to decimal
+        """
+
+        res = self.rules[s[-1]]
+
+        for i in range(len(s) - 1):
+            if self.rules[s[i]] < self.rules[s[i + 1]]:
+                res -= self.rules[s[i]]
             else:
-                res += rules[s[i]]
+                res += self.rules[s[i]]
 
         return res
