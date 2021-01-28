8b97da348c91c385e21ad2b285a95e146d753250
Hoon94 <dleognsdl1@naver.com>
2021-01-28 23:48:08 +0900

    3Sum

---
+++ Leetcode/15 3Sum.py
@@ -0,0 +1,46 @@
+class Solution:
+    def threeSum(self, nums: list) -> list:
+        """[summary]
+
+        Args:
+            nums (List[int]): -105 <= nums[i] <= 105, 0 <= nums.length <= 3000
+
+        Returns:
+            List[List[int]]: Find all unique triplets in the array which gives the sum of zero.
+        """
+
+        result = set()
+        nums.sort()
+
+        n, p, z = [], [], []
+        for num in nums:
+            if num > 0:
+                p.append(num)
+            elif num < 0:
+                n.append(num)
+            else:
+                z.append(num)
+
+        N, P = set(n), set(p)
+
+        if z:
+            for num in P:
+                if -num in N:
+                    result.add((-num, 0, num))
+
+        if len(z) >= 3:
+            result.add((0, 0, 0))
+
+        for i in range(len(n)):
+            for j in range(i + 1, len(n)):
+                target = -(n[i] + n[j])
+                if target in P:
+                    result.add((n[i], n[j], target))
+
+        for i in range(len(p)):
+            for j in range(i + 1, len(p)):
+                target = -(p[i] + p[j])
+                if target in N:
+                    result.add((p[i], p[j], target))
+
+        return result
