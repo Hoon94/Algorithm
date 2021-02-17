d302b75c54b066e6ec2a9b41f3797af8eaffa14d
Hoon94 <dleognsdl1@naver.com>
2021-02-17 23:54:45 +0900

    Merge k Sorted Lists

--- Leetcode/23. Merge k Sorted Lists.py
+++ Leetcode/23. Merge k Sorted Lists.py
@@ -1,5 +1,21 @@
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        """[summary]
+            You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+        Args:
+            lists (List[ListNode]): k == lists.length, 0 <= k <= 10^4, 0 <= lists[i].length <= 500,
+                                    -10^4 <= lists[i][j] <= 10^4, lists[i] is sorted in ascending order.
+                                    The sum of lists[i].length won't exceed 10^4.
+
+        Returns:
+            ListNode: Merge all the linked-lists into one sorted linked-list and return it.
+
+        Result:
+            Runtime: 116 ms, faster than 51.23% of Python3 online submissions for Merge k Sorted Lists.
+            Memory Usage: 23.7 MB, less than 5.03% of Python3 online submissions for Merge k Sorted Lists.
+        """
+
         def listVals(node):
             while node:
                 yield node.val
