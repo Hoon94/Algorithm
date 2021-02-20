f015ed66825bc3bfa023c36442eae6391b209d28
Hoon94 <dleognsdl1@naver.com>
2021-02-20 20:04:23 +0900

    Swap Nodes in Pairs

--- Leetcode/24. Swap Nodes in Pairs.py
+++ Leetcode/24. Swap Nodes in Pairs.py
@@ -1,21 +1,40 @@
+from typing import List
+
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
+        """[summary]
+            Given a linked list, swap every two adjacent nodes and return its head.
+
+        Args:
+            head (ListNode): 0 <= Node.val <= 100, The number of nodes in the list is in the range [0, 100].
+
+        Returns:
+            ListNode: head
+
+        Result:
+            Runtime: 20 ms, faster than 99.23% of Python3 online submissions for Swap Nodes in Pairs.
+            Memory Usage: 14.2 MB, less than 80.37% of Python3 online submissions for Swap Nodes in Pairs.
+        """
+
         if not head:
             return head
 
-        p1 = head
-        p2 = head.next
+        l1 = head
+        l2 = head.next
 
-        while p2:
-            p1.val, p2.val = p2.val, p1.val
-            if not p2.next:
+        while l2:
+            l1.val, l2.val = l2.val, l1.val
+            if not l2.next:
                 break
-            p1 = p1.next.next
-            p2 = p2.next.next
+            l1 = l1.next.next
+            l2 = l2.next.next
 
         return head
