7acf225919959c99f18ff1abff8f78fedf669ee9
Hoon94 <dleognsdl1@naver.com>
2021-02-21 03:32:25 +0900

    Reverse Nodes in k-Group

--- Leetcode/25. Reverse Nodes in k-Group.py
+++ Leetcode/25. Reverse Nodes in k-Group.py
@@ -1,10 +1,33 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        """[summary]
+            Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+            k is a positive integer and is less than or equal to the length of the linked list.
+            If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+            Follow up:
+            Could you solve the problem in O(1) extra memory space?
+            You may not alter the values in the list's nodes, only nodes itself may be changed.
+
+        Args:
+            head (ListNode): The number of nodes in the list is in the range sz. 1 <= sz <= 5000, 0 <= Node.val <= 1000
+            k (int): 1 <= k <= sz
+
+        Returns:
+            ListNode: reverse the nodes of a linked list k at a time and return its modified list.
+
+        Result:
+            Runtime: 40 ms, faster than 98.48% of Python3 online submissions for Reverse Nodes in k-Group.
+            Memory Usage: 15.3 MB, less than 17.16% of Python3 online submissions for Reverse Nodes in k-Group.
+        """
+
         if head is None or k == 1:
             return head
 
