bf0f843cefa801b480d88dcb2d4a349b67261e04
Hoon94 <dleognsdl1@naver.com>
2021-03-21 23:50:24 +0900

    Search in Rotated Sorted Array

--- Leetcode/33. Search in Rotated Sorted Array.py
+++ Leetcode/33. Search in Rotated Sorted Array.py
@@ -1,12 +1,28 @@
+from typing import List
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        """[summary]
+
+        Args:
+            nums (List[int]): [description]
+            target (int): [description]
+
+        Returns:
+            int: [description]
+
+        Result:
+            Runtime: 40 ms, faster than 76.50% of Python3 online submissions for Search in Rotated Sorted Array.
+            Memory Usage: 14.8 MB, less than 24.17% of Python3 online submissions for Search in Rotated Sorted Array.
+        """
         if not nums:
             return -1
 
         low, high = 0, len(nums) - 1
 
         while low <= high:
-            mid = (low + high) / 2
+            mid = (low + high) // 2
             if target == nums[mid]:
                 return mid
 
