65ce914ee9103e752f074a4ef6d5a587131434c5
Hoon94 <dleognsdl1@naver.com>
2021-03-30 00:25:47 +0900

    문자열 압축

--- python-for-coding-test/12_Implementation/9.py
+++ python-for-coding-test/12_Implementation/9.py
@@ -1,12 +1,12 @@
 def solution(s):
     answer = float("INF")
-    l = len(s) // 2
+    length = len(s) // 2
     count = 1
 
-    if len(s) <= 1:
+    if len(s) == 1:
         return len(s)
 
-    for i in range(1, l + 1):
+    for i in range(1, length + 1):
         string = ''
         for j in range(i, len(s), i):
             if s[j-i:j] == s[j:j+i]:
