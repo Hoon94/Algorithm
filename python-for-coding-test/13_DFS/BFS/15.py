b6e3a3e46fe8d7ed6eb47a32e4e9dfaf9e67ad1a
Hoon94 <dleognsdl1@naver.com>
2021-04-06 23:13:08 +0900

    특정 거리의 도시 찾기

--- python-for-coding-test/13_DFS/BFS/15.py
+++ python-for-coding-test/13_DFS/BFS/15.py
@@ -1,14 +1,14 @@
-from collections import deque
+from collections import deque, defaultdict
 import sys
 input = sys.stdin.readline
 
 n, m, k, x = map(int, input().split())
-graph = dict()
+graph = defaultdict(list)
 answer = []
 
 for _ in range(m):
     start, end = map(int, input().split())
-    graph[start] = graph.get(start, []) + [end]
+    graph[start] += [end]
 
 queue = deque([(x, 0)])
 visited = [x]
@@ -16,7 +16,7 @@ visited = [x]
 while queue:
     start, distance = queue.popleft()
     distance += 1
-    for i in graph.get(start, []):
+    for i in graph[start]:
         if i not in visited:
             if distance == k:
                 answer.append(i)
