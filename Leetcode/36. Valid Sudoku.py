5ba93754017c91c2d744f79d12b1a1fde55d583b
Hoon94 <dleognsdl1@naver.com>
2021-04-11 23:45:57 +0900

    Valid Sudoku

--- Leetcode/36. Valid Sudoku.py
+++ Leetcode/36. Valid Sudoku.py
@@ -3,6 +3,26 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        """[summary]
+        Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+        1. Each row must contain the digits 1-9 without repetition.
+        2. Each column must contain the digits 1-9 without repetition.
+        3. Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+        Note:
+        A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+        Only the filled cells need to be validated according to the mentioned rules.
+
+        Args:
+            board (List[List[str]]): board.length == 9, board[i].length == 9, board[i][j] is a digit or '.'.
+
+        Returns:
+            bool: Determine if Sudoku board is valid.
+
+        Result:
+            Runtime: 80 ms, faster than 99.68% of Python3 online submissions for Valid Sudoku.
+            Memory Usage: 14.2 MB, less than 71.49% of Python3 online submissions for Valid Sudoku.
+        """
 
         rows = [set() for i in range(9)]
         cols = [set() for i in range(9)]
@@ -12,7 +32,6 @@ class Solution:
             for j in range(9):
                 cur = board[i][j]
                 if cur != '.':
-
                     k = (i // 3) * 3 + j // 3
 
                     if cur not in rows[i]:
@@ -29,4 +48,5 @@ class Solution:
                         mMat[k].add(cur)
                     else:
                         return False
+
         return True
