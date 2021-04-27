7c70583b9322a62d42a940e95423d484db9132b5
Hoon94 <dleognsdl1@naver.com>
2021-04-27 23:09:21 +0900

    인구 이동

--- python-for-coding-test/13_DFS/BFS/21.py
+++ python-for-coding-test/13_DFS/BFS/21.py
@@ -1,56 +1,48 @@
-from collections import deque
 import sys
-input = sys.stdin.readline
+sys.setrecursionlimit(100000)
 
 n, l, r = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+maps = [list(map(int, input().split())) for _ in range(n)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-def bfs(row, col):
-    check = False
-    visited = {(row, col)}
-    q = deque([(row, col)])
-    val, cnt = 0, 0
-
-    while q:
-        x, y = q.popleft()
-        val += arr[x][y]
-        cnt += 1
-        for i in range(4):
-            nx = x + dy[i]
-            ny = y + dx[i]
-
-            if 0 <= nx < n and 0 <= ny < n and (nx, ny) not in visited and l <= abs(arr[nx][ny] - arr[x][y]) <= r:
-                q.append((nx, ny))
-                visited.add((nx, ny))
-                check = True
-    return val // cnt, visited, check
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+count = 0
 
 
-def move():
-    cnt = 0
-    while True:
-        is_move = False
-        total_visited = set()
-        temp = []
-        for i in range(n):
-            for j in range(n):
-                if (i, j) not in total_visited:
-                    value, visited, flag = bfs(i, j)
-                    if flag:
-                        is_move = True
-                    temp.append((value, visited))
-                    total_visited |= visited
+def go(x, y):
+    for i in range(4):
+        new_x = x + dx[i]
+        new_y = y + dy[i]
 
-        for (value, visit) in temp:
-            for y, x in visit:
-                arr[y][x] = value
-        if not is_move:
-            return cnt
-        cnt += 1
+        if 0 <= new_x < n and 0 <= new_y < n and check[new_x][new_y]:
+            temp = abs(maps[x][y] - maps[new_x][new_y])
+            if l <= temp <= r:
+                check[new_x][new_y] = False
+                stack.append([new_x, new_y])
+                go(new_x, new_y)
 
 
-print(move())
+while True:
+    check = [[True] * n for _ in range(n)]
+    flag = True
+    for i in range(n):
+        for j in range(n):
+            stack = []
+
+            if check[i][j]:
+                stack.append([i, j])
+                check[i][j] = False
+                go(i, j)
+
+                if len(stack) > 1:
+                    flag = False
+                    avg = sum([maps[x][y] for x, y in stack]) // len(stack)
+                    for x, y in stack:
+                        maps[x][y] = avg
+
+    if flag:
+        break
+
+    count += 1
+
+print(count)
