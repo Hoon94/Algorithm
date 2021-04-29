692f261070ac1ddd815cb4f5436ad6fa169c5048
Hoon94 <dleognsdl1@naver.com>
2021-04-29 22:38:47 +0900

    블록 이동하기

--- python-for-coding-test/13_DFS/BFS/22.py
+++ python-for-coding-test/13_DFS/BFS/22.py
@@ -1,50 +1,50 @@
 from collections import deque
 
 
-def can_move(cur1, cur2, new_board):
-    Y, X = 0, 1
+def can_move(p1, p2, new_board):
+    y, x = 0, 1
     cand = []
     # 평행이동
-    DELTAS = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-    for dy, dx in DELTAS:
-        nxt1 = (cur1[Y] + dy, cur1[X] + dx)
-        nxt2 = (cur2[Y] + dy, cur2[X] + dx)
-        if new_board[nxt1[Y]][nxt1[X]] == 0 and new_board[nxt2[Y]][nxt2[X]] == 0:
+    move = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+    for dy, dx in move:
+        nxt1 = (p1[y] + dy, p1[x] + dx)
+        nxt2 = (p2[y] + dy, p2[x] + dx)
+        if new_board[nxt1[y]][nxt1[x]] == 0 and new_board[nxt2[y]][nxt2[x]] == 0:
             cand.append((nxt1, nxt2))
+
     # 회전
-    if cur1[Y] == cur2[Y]:  # 가로방향 일 때
-        UP, DOWN = -1, 1
-        for d in [UP, DOWN]:
-            if new_board[cur1[Y]+d][cur1[X]] == 0 and new_board[cur2[Y]+d][cur2[X]] == 0:
-                cand.append((cur1, (cur1[Y]+d, cur1[X])))
-                cand.append((cur2, (cur2[Y]+d, cur2[X])))
+    if p1[y] == p2[y]:  # 가로 방향 일 때
+        up, down = -1, 1
+        for d in [up, down]:
+            if new_board[p1[y] + d][p1[x]] == 0 and new_board[p2[y] + d][p2[x]] == 0:
+                cand.append((p1, (p1[y] + d, p1[x])))
+                cand.append((p2, (p2[y] + d, p2[x])))
     else:  # 세로 방향 일 때
-        LEFT, RIGHT = -1, 1
-        for d in [LEFT, RIGHT]:
-            if new_board[cur1[Y]][cur1[X]+d] == 0 and new_board[cur2[Y]][cur2[X]+d] == 0:
-                cand.append(((cur1[Y], cur1[X]+d), cur1))
-                cand.append(((cur2[Y], cur2[X]+d), cur2))
+        left, right = -1, 1
+        for d in [left, right]:
+            if new_board[p1[y]][p1[x] + d] == 0 and new_board[p2[y]][p2[x] + d] == 0:
+                cand.append(((p1[y], p1[x] + d), p1))
+                cand.append(((p2[y], p2[x] + d), p2))
 
     return cand
 
 
 def solution(board):
-    # board 외벽 둘러싸기
-    N = len(board)
-    new_board = [[1] * (N+2) for _ in range(N+2)]
-    for i in range(N):
-        for j in range(N):
-            new_board[i+1][j+1] = board[i][j]
+    n = len(board)
+    new_board = [[1] * (n + 2) for _ in range(n + 2)]
+    print(new_board)
+    for i in range(n):
+        for j in range(n):
+            new_board[i + 1][j + 1] = board[i][j]
 
-    # 현재 좌표 위치 큐 삽입, 확인용 set
-    que = deque([((1, 1), (1, 2), 0)])
-    confirm = set([((1, 1), (1, 2))])
+    q = deque([((1, 1), (1, 2), 0)])
+    visited = set([((1, 1), (1, 2))])
 
-    while que:
-        cur1, cur2, count = que.popleft()
-        if cur1 == (N, N) or cur2 == (N, N):
+    while q:
+        p1, p2, count = q.popleft()
+        if p1 == (n, n) or p2 == (n, n):
             return count
-        for nxt in can_move(cur1, cur2, new_board):
-            if nxt not in confirm:
-                que.append((*nxt, count+1))
-                confirm.add(nxt)
+        for nxt in can_move(p1, p2, new_board):
+            if nxt not in visited:
+                q.append((*nxt, count + 1))
+                visited.add(nxt)
