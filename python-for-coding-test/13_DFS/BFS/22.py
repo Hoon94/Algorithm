787c66b5ee3f9f2f09a20d836f3e9dbe9cc5a631
Hoon94 <dleognsdl1@naver.com>
2021-04-30 02:44:33 +0900

    블록 이동하기

--- python-for-coding-test/13_DFS/BFS/22.py
+++ python-for-coding-test/13_DFS/BFS/22.py
@@ -1,50 +1,46 @@
 from collections import deque
 
 
-def can_move(p1, p2, new_board):
-    y, x = 0, 1
-    cand = []
-    # 평행이동
-    move = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-    for dy, dx in move:
-        nxt1 = (p1[y] + dy, p1[x] + dx)
-        nxt2 = (p2[y] + dy, p2[x] + dx)
-        if new_board[nxt1[y]][nxt1[x]] == 0 and new_board[nxt2[y]][nxt2[x]] == 0:
-            cand.append((nxt1, nxt2))
-
-    # 회전
-    if p1[y] == p2[y]:  # 가로 방향 일 때
-        up, down = -1, 1
-        for d in [up, down]:
-            if new_board[p1[y] + d][p1[x]] == 0 and new_board[p2[y] + d][p2[x]] == 0:
-                cand.append((p1, (p1[y] + d, p1[x])))
-                cand.append((p2, (p2[y] + d, p2[x])))
-    else:  # 세로 방향 일 때
-        left, right = -1, 1
-        for d in [left, right]:
-            if new_board[p1[y]][p1[x] + d] == 0 and new_board[p2[y]][p2[x] + d] == 0:
-                cand.append(((p1[y], p1[x] + d), p1))
-                cand.append(((p2[y], p2[x] + d), p2))
-
-    return cand
+def getNewPos(pos, board):
+    dir = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    newPos = []
+    y1, x1 = pos[0]
+    y2, x2 = pos[1]
+    # 상하좌우 이동
+    for dy, dx in dir:
+        if board[y1+dy][x1+dx] == 0 and board[y2+dy][x2+dx] == 0:
+            newPos.append({(y1+dy, x1+dx), (y2+dy, x2+dx)})
+    # 가로 -> 세로
+    if y1 == y2:
+        for dy, dx in dir[1::2]:
+            if board[y1+dy][x1+dx] == 0 and board[y2+dy][x2+dx] == 0:
+                newPos.append({(y1, x1), (y1+dy, x1)})
+                newPos.append({(y2, x2), (y2+dy, x2)})
+    # 세로 -> 가로
+    else:
+        for dy, dx in dir[::2]:
+            if board[y1+dy][x1+dx] == 0 and board[y2+dy][x2+dx] == 0:
+                newPos.append({(y1, x1), (y1, x1+dx)})
+                newPos.append({(y2, x2), (y2, x2+dx)})
+    return newPos
 
 
 def solution(board):
     n = len(board)
-    new_board = [[1] * (n + 2) for _ in range(n + 2)]
-    for i in range(n):
-        for j in range(n):
-            new_board[i + 1][j + 1] = board[i][j]
-
-    q = deque([((1, 1), (1, 2), 0)])
-    visited = set([((1, 1), (1, 2))])
-
+    board = [[1] + b + [1] for b in board]
+    board = [[1] * (n + 2)] + board + [[1] * (n + 2)]
+    pos = {(1, 1), (1, 2)}
+    q = deque()
+    visited = []
+    q.append([pos, 0])
+    visited.append(pos)
     while q:
-        p1, p2, count = q.popleft()
-        if p1 == (n, n) or p2 == (n, n):
-            print(visited)
-            return count
-        for nxt in can_move(p1, p2, new_board):
-            if nxt not in visited:
-                q.append((*nxt, count + 1))
-                visited.add(nxt)
+        pos, distance = q.popleft()
+        distance += 1
+        for newPos in getNewPos(list(pos), board):
+            if (n, n) in newPos:
+                return distance
+            if newPos not in visited:
+                q.append([newPos, distance])
+                visited.append(newPos)
+    return -1
