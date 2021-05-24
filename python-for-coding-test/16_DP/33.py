016ee62b3912cde3939fad34fedeb67a5c3e0005
Hoon94 <dleognsdl1@naver.com>
2021-05-24 23:36:33 +0900

    퇴사

--- python-for-coding-test/16_DP/33.py
+++ python-for-coding-test/16_DP/33.py
@@ -1,13 +1,51 @@
-import sys
+# N+1일째 되는 날 퇴사를 하기 위해 남은 N일동안 최대한 많은 상담을 함
+N = int(input())
+# 상담을 완료하는데 걸리는 기간 T와 상담을 했을 때 받을 수 있는 금액 P
+T = []
+P = []
+for i in range(N):
+    a, b = map(int, input().split(' '))
+    T.append(a)
+    P.append(b)
 
-n = int(input())
-timeTable = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-dp = [0 for _ in range(n + 1)]
+# i 번째 날까지 일을 했을 때 최대값을 보관할 dp 배열
+dp = [0 for _ in range(N+2)]
 
-for i in range(n - 1, -1, -1):
-    if i + timeTable[i][0] > n:
-        dp[i] = dp[i + 1]
-    else:
-        dp[i] = max(timeTable[i][1] + dp[i + timeTable[i][0]], dp[i + 1])
+# for i in range(N - 1, -1, -1):
+#     # 오늘 날짜가 가능한지 안한지 비교후 선택
+#     if T[i] + i <= N:
+#         # 돈 P[i] + 날짜 dp[i + T[i]], 그리고 다음날의 값과 비교
+#         dp[i] = max(P[i] + dp[i + T[i]], dp[i+1])
+#     # 날짜 선택하지 않을 경우
+#     else:
+#         dp[i] = dp[i+1]
 
-print(dp[0])
+for i in range(N):
+    if dp[i] > dp[i+1]:
+        dp[i+1] = dp[i]
+
+    if i + T[i] <= N:
+        if dp[i + T[i]] < P[i] + dp[i]:
+            dp[i + T[i]] = P[i] + dp[i]
+
+ans = 0
+if dp[N] > dp[N-1]:
+    ans = dp[N]
+else:
+    ans = dp[N-1]
+print(dp)
+
+
+# import sys
+
+# n = int(input())
+# timeTable = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+# dp = [0 for _ in range(n + 1)]
+
+# for i in range(n - 1, -1, -1):
+#     if i + timeTable[i][0] > n:
+#         dp[i] = dp[i + 1]
+#     else:
+#         dp[i] = max(timeTable[i][1] + dp[i + timeTable[i][0]], dp[i + 1])
+
+# print(dp[0])
