60ef859bbdaafe4f3f602de101990135d9f9d1fc
Hoon94 <dleognsdl1@naver.com>
2021-05-25 22:36:16 +0900

    병사 배치하기

--- python-for-coding-test/16_DP/34.py
+++ python-for-coding-test/16_DP/34.py
@@ -1,10 +1,10 @@
 n = int(input())
-arr = list(map(int, input().split()))
-answer = [1] * n
+array = list(map(int, input().split()))
+dp = [1] * n
 
 for i in range(n):
-    for a in range(len(arr[:i])):
-        if arr[i] < arr[a] and answer[i] < answer[a] + 1:
-            answer[i] = answer[a] + 1
+    for j in range(i):
+        if array[j] > array[i] and dp[j] + 1 > dp[i]:
+            dp[i] = dp[j] + 1
 
-print(n - max(answer))
+print(n - max(dp))
