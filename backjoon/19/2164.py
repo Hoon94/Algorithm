d3ef6714b3d5e5d5d120bced6a18dee9523859c0
Hoon94 <dleognsdl1@naver.com>
2021-07-18 01:34:56 +0900

    카드2

--- backjoon/19/2164.py
+++ backjoon/19/2164.py
@@ -1,14 +1,14 @@
 import sys
-from collections import deque
 
 N = int(sys.stdin.readline())
-queue = deque()
+arr = [i + 1 for i in range(N)]
 
-for i in range(N):
-    queue.append(i + 1)
+while len(arr) > 1:
+    if len(arr) % 2:
+        t = [arr[-1]]
+        t.extend(arr[1::2])
+        arr = t
+    else:
+        arr = arr[1::2]
 
-while len(queue) > 1:
-    queue.popleft()
-    queue.append(queue.popleft())
-
-print(queue.pop())
+print(arr[0])
