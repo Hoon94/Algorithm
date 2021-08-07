bfc12889a2fa8dcfd291ca6f330d9d7fd03d8fb2
Hoon94 <dleognsdl1@naver.com>
2021-08-07 19:49:31 +0900

    숫자 카드 2

--- backjoon/21/10816.py
+++ backjoon/21/10816.py
@@ -1,28 +1,15 @@
 from sys import stdin
+
 _ = stdin.readline()
-N = sorted(map(int, stdin.readline().split()))
+N = map(int, stdin.readline().split())
 _ = stdin.readline()
 M = map(int, stdin.readline().split())
-
-
-def binary(n, N, start, end):
-    if start > end:
-        return 0
-    m = (start + end) // 2
-    if n == N[m]:
-        return N[start:end + 1].count(n)
-    elif n < N[m]:
-        return binary(n, N, start, m - 1)
-    else:
-        return binary(n, N, m + 1, end)
-
-
-n_dic = {}
+hashmap = {}
 
 for n in N:
-    start = 0
-    end = len(N) - 1
-    if n not in n_dic:
-        n_dic[n] = binary(n, N, start, end)
+    if n in hashmap:
+        hashmap[n] += 1
+    else:
+        hashmap[n] = 1
 
-print(' '.join(str(n_dic[x]) if x in n_dic else '0' for x in M))
+print(' '.join(str(hashmap[m]) if m in hashmap else '0' for m in M))
