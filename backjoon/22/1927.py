67394d81525c56ca744466ac102c6c88c5a9fe02
Hoon94 <dleognsdl1@naver.com>
2021-08-15 21:04:01 +0900

    최소 힙

---
+++ backjoon/22/1927.py
@@ -0,0 +1,16 @@
+import heapq
+from sys import stdin
+
+N = int(stdin.readline())
+heap = []
+
+for i in range(N):
+    num = int(stdin.readline())
+
+    if num == 0:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, [num, num])
