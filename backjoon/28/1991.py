01cd05855e78c741a1f4aef2c17893ecb0755e37
Hoon94 <dleognsdl1@naver.com>
2021-10-19 20:42:28 +0900

    트리 순회

--- backjoon/28/1991.py
+++ backjoon/28/1991.py
@@ -1,41 +1,47 @@
 import sys
-
 input = sys.stdin.readline
+
+
+class Node:
+    def __init__(self, item, left, right):
+        self.item = item
+        self.left = left
+        self.right = right
+
+
+def preorder(node):
+    print(node.item, end="")
+    if node.left != '.':
+        preorder(tree[node.left])
+    if node.right != '.':
+        preorder(tree[node.right])
+
+
+def inorder(node):
+    if node.left != '.':
+        inorder(tree[node.left])
+    print(node.item, end="")
+    if node.right != '.':
+        inorder(tree[node.right])
+
+
+def postorder(node):
+    if node.left != '.':
+        postorder(tree[node.left])
+    if node.right != '.':
+        postorder(tree[node.right])
+    print(node.item, end="")
+
+
 n = int(input())
-s = [[0] * 3 for _ in range(26)]
+tree = {}
 
-
-def preorder(a):
-    print(a, end="")
-    if s[ord(a)-65][1] != ".":
-        preorder(s[ord(a) - 65][1])
-    if s[ord(a)-65][2] != ".":
-        preorder(s[ord(a) - 65][2])
-
-
-def inorder(a):
-    if s[ord(a)-65][1] != ".":
-        inorder(s[ord(a) - 65][1])
-    print(a, end="")
-    if s[ord(a)-65][2] != ".":
-        inorder(s[ord(a) - 65][2])
-
-
-def postorder(a):
-    if s[ord(a)-65][1] != ".":
-        postorder(s[ord(a) - 65][1])
-    if s[ord(a)-65][2] != ".":
-        postorder(s[ord(a) - 65][2])
-    print(a, end="")
-
-
-for i in range(n):
+for _ in range(n):
     node, left, right = map(str, input().split())
-    item = ord(node) - 65
-    s[item][0], s[item][1], s[item][2] = node, left, right
+    tree[node] = Node(item=node, left=left, right=right)
 
-preorder("A")
+preorder(tree['A'])
 print()
-inorder("A")
+inorder(tree['A'])
 print()
-postorder("A")
+postorder(tree['A'])
