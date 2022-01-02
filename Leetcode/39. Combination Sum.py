6102e99c17ce7e711f3f373ed54c73e1bdd273b6
Hoon94 <dleognsdl1@naver.com>
2022-01-02 14:30:52 +0900

    Combination Sum

---
+++ Leetcode/39. Combination Sum.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+        candidates.sort()
+
+        def DFS(arr, sm, target, j):
+            if sm == target:
+                if arr not in ans:
+                    ans.append(arr)
+                return
+
+            for i in range(j, len(candidates)):
+                if sm + candidates[i] > target:
+                    return
+
+                DFS(arr + [candidates[i]], sm + candidates[i], target, i)
+
+        DFS([], 0, target, 0)
+
+        return ans
