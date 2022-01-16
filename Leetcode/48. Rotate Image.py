5c3986f7be0b059ff62732fc0851e116b4b182c4
Hoon94 <dleognsdl1@naver.com>
2022-01-16 22:31:07 +0900

    Rotate Image

--- Leetcode/48. Rotate Image.py
+++ Leetcode/48. Rotate Image.py
@@ -3,7 +3,12 @@ from typing import List
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        matrix.reverse()
-        for i in range(len(matrix)):
+        n = len(matrix)
+
+        for i in range(n):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for row in matrix:
+            for j in range(n / 2):
+                row[j], row[~j] = row[~j], row[j]
