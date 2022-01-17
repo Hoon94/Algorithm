59564bb10389e79fdc436c2d5cc54a67acd6839b
Hoon94 <dleognsdl1@naver.com>
2022-01-17 22:01:58 +0900

    Group Anagrams

---
+++ Leetcode/49. Group Anagrams.py
@@ -0,0 +1,12 @@
+from typing import List
+import collections
+
+
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    dic = collections.defaultdict(list)
+
+    for st in strs:
+        s = ''.join(sorted(st))
+        dic[s].append(st)
+
+    return dic.values()
