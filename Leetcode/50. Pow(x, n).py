0d2a6eb8dfdde7710abd1d79f082ff85f3b5c84d
Hoon94 <dleognsdl1@naver.com>
2022-01-19 22:02:05 +0900

    Pow(x, n)

--- Leetcode/50. Pow(x, n).py
+++ Leetcode/50. Pow(x, n).py
@@ -1,10 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        if n >= 0:
+            return self.helper(x, n)
+        else:
+            return 1 / self.helper(x, -n)
+
+    def helper(self, x, n):
         if n == 0:
             return 1
-        if n < 0:
-            n, x = -n, 1 / x
 
-        lower = self.myPow(x, n // 2)
+        temp = self.myPow(x, n // 2)
 
-        return lower * lower * x if n % 2 else lower * lower
+        if int(n % 2) == 0:
+            return temp * temp
+        else:
+            return temp * temp * x
