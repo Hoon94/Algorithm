f24ef4beaab1f371ed5ebb55f0bc7cbce6a7ab29
Hoon94 <dleognsdl1@naver.com>
2022-01-24 20:58:08 +0900

    Jump Game

---
+++ Leetcode/55. Jump Game.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def canJump(self, nums: List[int]) -> bool:
+    j = 0
+
+    for i in range(len(nums)):
+        if i > j:
+            return False
+        j = max(j, i + nums[i])
+
+    return True
