7e8f177fd5b9f92592ae6060c8dcb9ff84977b29
Hoon94 <dleognsdl1@naver.com>
2022-01-27 20:08:45 +0900

    Insert Interval

--- Leetcode/57. Insert Interval.py
+++ Leetcode/57. Insert Interval.py
@@ -1,27 +1,19 @@
 from typing import List
-import heapq
 
 
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        heap, ans, = [], []
+        result = []
 
-        for s, e in intervals + [newInterval]:
-            heapq.heappush(heap, (s, -1))
-            heapq.heappush(heap, (e, 1))
+        for interval in intervals:
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+            elif interval[0] > newInterval[1]:
+                result.append(newInterval)
+                newInterval = interval
+            elif interval[1] >= newInterval[0] or interval[0] <= newInterval[1]:
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
 
-        cur, s = 0, None
-
-        while heap:
-            i, val = heapq.heappop(heap)
-
-            if s is None:
-                s = i
-
-            cur += val
-
-            if not cur:
-                ans.append([s, i])
-                s = None
-
-        return ans
+        result.append(newInterval)
+        return result
