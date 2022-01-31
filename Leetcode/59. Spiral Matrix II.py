8db6f53fa308c29bb531a16000a3dbe461ba4bea
Hoon94 <dleognsdl1@naver.com>
2022-01-31 20:22:44 +0900

    Spiral Matrix II

--- Leetcode/59. Spiral Matrix II.py
+++ Leetcode/59. Spiral Matrix II.py
@@ -3,10 +3,9 @@ from typing import List
 
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        A, lo = [], n * n + 1
+        A = [[n * n]]
 
-        while lo > 1:
-            lo, hi = lo - len(A), lo
-            A = [range(lo, hi)] + zip(*A[::-1])
+        while A[0][0] > 1:
+            A = [range(A[0][0] - len(A), A[0][0])] + zip(*A[::-1])
 
-        return A
+        return A * (n > 0)
