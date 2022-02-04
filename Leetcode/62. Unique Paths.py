5da500154cfdb7715192494a10ffff0a5bf1a777
Hoon94 <dleognsdl1@naver.com>
2022-02-04 23:46:42 +0900

    Unique Paths

--- Leetcode/62. Unique Paths.py
+++ Leetcode/62. Unique Paths.py
@@ -1,10 +1,8 @@
+from math import factorial
+
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         rows, cols = m, n
-        path_dp = [[1 for _ in range(cols)] for _ in range(rows)]
 
-        for i in range(1, rows):
-            for j in range(1, cols):
-                path_dp[i][j] = path_dp[i][j - 1] + path_dp[i - 1][j]
-
-        return path_dp[rows - 1][cols - 1]
+        return factorial(rows + cols - 2) // (factorial(rows - 1) * factorial(cols - 1))
