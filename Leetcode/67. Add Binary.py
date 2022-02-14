a0667d97e849e780061a0e1f59ab3dc32ab18380
Hoon94 <dleognsdl1@naver.com>
2022-02-14 20:57:32 +0900

    Add Binary

--- Leetcode/67. Add Binary.py
+++ Leetcode/67. Add Binary.py
@@ -1,27 +1,9 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        while len(b) < len(a):
-            b = '0' + b
-        while len(a) < len(b):
-            a = '0' + a
+        a, b, carry, ans = list(map(int, a)), list(map(int, b)), 0, []
+        while a or b or carry:
+            carry, res = divmod((a.pop() if a else 0) +
+                                (b.pop() if b else 0) + carry, 2)
+            ans.append(str(res))
 
-        carry = 0
-        a = [int(i) for i in a]
-        b = [int(i) for i in b]
-        ans = ''
-
-        for i in range(len(a) - 1, -1, -1):
-            val = a[i] + b[i] + carry
-            if val == 3:
-                carry = 1
-                ans = '1' + ans
-            elif val == 2:
-                carry = 1
-                ans = '0' + ans
-            elif val == 1:
-                ans = '1' + ans
-                carry = 0
-            else:
-                ans = '0' + ans
-
-        return str(carry) + ans if carry else ans
+        return ''.join(ans[::-1])
