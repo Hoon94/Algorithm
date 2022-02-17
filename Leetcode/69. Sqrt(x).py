688dde2b0b210568d9c0b49921dbd4da0b175fae
Hoon94 <dleognsdl1@naver.com>
2022-02-17 21:10:10 +0900

    Sqrt(x)

--- Leetcode/69. Sqrt(x).py
+++ Leetcode/69. Sqrt(x).py
@@ -3,13 +3,14 @@ class Solution:
         low = 0
         high = x
 
-        while(low <= high):
+        while low <= high:
             mid = low + (high - low) // 2
-            if (mid * mid <= x and (mid + 1) * (mid + 1) > x):
+            if mid * mid == x:
                 return mid
             elif mid * mid < x:
+                ans = mid
                 low = mid + 1
             else:
                 high = mid - 1
 
-        return low
+        return ans
