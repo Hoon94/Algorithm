427c4d2834a08f273a061dbf5b6855ce1e7ca8f2
Hoon94 <dleognsdl1@naver.com>
2022-02-19 13:08:30 +0900

    Climbing Stairs

--- Leetcode/70. Climbing Stairs.py
+++ Leetcode/70. Climbing Stairs.py
@@ -1,12 +1,14 @@
 class Solution(object):
     def climbStairs(self, n):
+        memo = {}
+        memo[1] = 1
+        memo[2] = 2
+
         def climb(n):
-            if n == 1:
-                return 1
-
-            if n == 2:
-                return 2
-
-            return climb(n - 1) + climb(n - 2)
+            if n in memo:
+                return memo[n]
+            else:
+                memo[n] = climb(n - 1) + climb(n - 2)
+                return memo[n]
 
         return climb(n)
