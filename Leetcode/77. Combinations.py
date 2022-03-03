dbe4ed3b0d41cfa0e551cb989dbb4271bae57609
Hoon94 <dleognsdl1@naver.com>
2022-03-03 18:31:24 +0900

    Combinations

--- Leetcode/77. Combinations.py
+++ Leetcode/77. Combinations.py
@@ -3,7 +3,9 @@ from typing import List
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        if k == 0:
-            return [[]]
+        combs = [[]]
+        for _ in range(k):
+            combs = [
+                [i] + c for c in combs for i in range(1, c[0] if c else n + 1)]
 
-        return [pre + [i] for i in range(k, n + 1) for pre in self.combine(i - 1, k - 1)]
+        return combs
