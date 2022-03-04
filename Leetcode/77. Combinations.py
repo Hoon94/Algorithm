ae3cef89e750b654748dab29d38d7a2a4e85de9c
Hoon94 <dleognsdl1@naver.com>
2022-03-04 19:10:20 +0900

    Combinations

--- Leetcode/77. Combinations.py
+++ Leetcode/77. Combinations.py
@@ -1,11 +1,7 @@
 from typing import List
+from functools import reduce
 
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        combs = [[]]
-        for _ in range(k):
-            combs = [
-                [i] + c for c in combs for i in range(1, c[0] if c else n + 1)]
-
-        return combs
+        return reduce(lambda C, _: [[i] + c for c in C for i in range(1, c[0] if c else n + 1)], range(k), [[]])
