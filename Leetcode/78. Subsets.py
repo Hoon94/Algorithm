1e6c858edc64caf677566af21ef1cfff684e0c9b
Hoon94 <dleognsdl1@naver.com>
2022-03-05 22:12:57 +0900

    Subsets

---
+++ Leetcode/78. Subsets.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = [[]]
+
+        for num in nums:
+            result += [i + [num] for i in result]
+
+        return result
