4fdfc7188bf61e1736db31bb03a1b434aae6b9ac
Hoon94 <dleognsdl1@naver.com>
2022-03-22 07:00:39 +0900

    Merge Sorted Array

--- Leetcode/88. Merge Sorted Array.py
+++ Leetcode/88. Merge Sorted Array.py
@@ -3,12 +3,18 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        while m > 0 and n > 0:
-            if nums1[m - 1] > nums2[n - 1]:
-                nums1[m + n - 1] = nums1[m - 1]
+        writeIndex = m + n - 1
+        m, n = m - 1, n - 1
+
+        while n >= 0 and m >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[writeIndex] = nums1[m]
                 m -= 1
             else:
-                nums1[m + n - 1] = nums2[n - 1]
+                nums1[writeIndex] = nums2[n]
                 n -= 1
 
-        nums1[:n] = nums2[:n]
+            writeIndex -= 1
+
+        if n > -1:
+            nums1[0:n + 1] = nums2[0:n + 1]
