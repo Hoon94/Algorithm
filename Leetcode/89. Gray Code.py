3a4b3b1131353c3755096d8e7a861216e7837477
Hoon94 <dleognsdl1@naver.com>
2022-03-24 19:54:15 +0900

    Gray Code

--- Leetcode/89. Gray Code.py
+++ Leetcode/89. Gray Code.py
@@ -3,13 +3,9 @@ from typing import List
 
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        if not n:
-            return [0]
+        res = [0]
 
-        res = [0, 1]
-
-        for i in range(2, n + 1):
-            for j in range(len(res) - 1, -1, -1):
-                res.append(res[j] | 1 << i - 1)
+        for i in range(1, 2 ** n):
+            res.append(res[-1] ^ (i & -i))
 
         return res
