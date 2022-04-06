3743582755b8e543ea6323fd1e78aba40ecd030d
Hoon94 <dleognsdl1@naver.com>
2022-04-06 23:12:56 +0900

    Unique Binary Search Trees

--- Leetcode/96. Unique Binary Search Trees.py
+++ Leetcode/96. Unique Binary Search Trees.py
@@ -1,16 +1,25 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        def countTrees(n):
+        cache = {}
+
+        def countTrees(n, cache):
             if n == 0:
                 return 1
             if n == 1:
                 return 1
 
+            if cache.get(n, -1) != -1:
+                return cache[n]
+
             Result = 0
 
             for i in range(n):
-                LeftTrees = countTrees(i)
-                RightTrees = countTrees(n - i - 1)
+                LeftTrees = countTrees(i, cache)
+                RightTrees = countTrees(n - i - 1, cache)
                 Result += LeftTrees * RightTrees
 
+            cache[n] = Result
+
             return Result
+
+        return countTrees(n, cache)
