7babd9b47301bd2b14da2b0327dd61127b86fe70
Hoon94 <dleognsdl1@naver.com>
2022-04-09 18:08:46 +0900

    Interleaving String

--- Leetcode/97. Interleaving String.py
+++ Leetcode/97. Interleaving String.py
@@ -1,22 +1,20 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        l1, l2, l3 = len(s1) + 1, len(s2) + 1, len(s3) + 1
+        r, c, l = len(s1), len(s2), len(s3)
 
-        if l1 + l2 != l3 + 1:
+        if r + c != l:
             return False
 
-        pre = [True for _ in range(l2)]
+        dp = [True for _ in range(c + 1)]
 
-        for j in range(1, l2):
-            pre[j] = pre[j - 1] and s2[j - 1] == s3[j - 1]
+        for j in range(1, c + 1):
+            dp[j] = dp[j - 1] and s2[j - 1] == s3[j - 1]
 
-        for i in range(1, l1):
-            cur = [pre[0] and s1[i - 1] == s3[i - 1]] * l2
+        for i in range(1, r + 1):
+            dp[0] = (dp[0] and s1[i - 1] == s3[i - 1])
 
-            for j in range(1, l2):
-                cur[j] = (cur[j - 1] and s2[j - 1] == s3[i + j - 1]) or \
-                    (pre[j] and s1[i - 1] == s3[i + j - 1])
+            for j in range(1, c + 1):
+                dp[j] = (dp[j] and s1[i - 1] == s3[i - 1 + j]) or \
+                    (dp[j - 1] and s2[j - 1] == s3[i - 1 + j])
 
-            pre = cur[:]
-
-        return pre[-1]
+        return dp[-1]
