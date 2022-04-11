9baa07bc38be235104a7756eec7c5b69e9a8e2b5
Hoon94 <dleognsdl1@naver.com>
2022-04-11 19:16:27 +0900

    Interleaving String

--- Leetcode/97. Interleaving String.py
+++ Leetcode/97. Interleaving String.py
@@ -5,20 +5,20 @@ class Solution:
         if r + c != l:
             return False
 
-        stack, visited = [(0, 0)], set((0, 0))
+        queue, visited = [(0, 0)], set((0, 0))
 
-        while stack:
-            x, y = stack.pop()
+        while queue:
+            x, y = queue.pop(0)
 
             if x + y == l:
                 return True
 
             if x + 1 <= r and s1[x] == s3[x + y] and (x + 1, y) not in visited:
-                stack.append((x + 1, y))
+                queue.append((x + 1, y))
                 visited.add((x + 1, y))
 
             if y + 1 <= c and s2[y] == s3[x + y] and (x, y + 1) not in visited:
-                stack.append((x, y + 1))
+                queue.append((x, y + 1))
                 visited.add((x, y + 1))
 
         return False
