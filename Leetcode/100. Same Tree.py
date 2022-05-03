7e7d962256115e842bee09d2b1728111125f098e
Hoon94 <dleognsdl1@naver.com>
2022-05-03 17:32:30 +0900

    Same Tree

--- Leetcode/100. Same Tree.py
+++ Leetcode/100. Same Tree.py
@@ -10,7 +10,4 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def t(n):
-            return n and (n.val, t(n.left), t(n.right))
-
-        return t(p) == t(q)
+        return p and q and p.val == q.val and all(map(self.isSameTree, (p.left, p.right), (q.left, q.right))) or p is q
