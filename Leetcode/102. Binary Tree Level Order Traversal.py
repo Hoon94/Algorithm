7451caa122090c61adac00ebe3b625d9402332e9
Hoon94 <dleognsdl1@naver.com>
2022-05-09 22:16:50 +0900

    Binary Tree Level Order Traversal

--- Leetcode/102. Binary Tree Level Order Traversal.py
+++ Leetcode/102. Binary Tree Level Order Traversal.py
@@ -14,7 +14,6 @@ class Solution:
 
         while root and level:
             ans.append([node.val for node in level])
-            LRpair = [(node.left, node.right) for node in level]
-            level = [leaf for LR in LRpair for leaf in LR if leaf]
+            level = [kid for n in level for kid in (n.left, n.right) if kid]
 
         return ans
