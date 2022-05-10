5a4207b7008e476c3e1e032e2795f478381cb7b7
Hoon94 <dleognsdl1@naver.com>
2022-05-10 19:35:14 +0900

    Binary Tree Level Order Traversal

--- Leetcode/102. Binary Tree Level Order Traversal.py
+++ Leetcode/102. Binary Tree Level Order Traversal.py
@@ -10,10 +10,18 @@ class TreeNode:
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
         ans, level = [], [root]
 
-        while root and level:
+        while level:
             ans.append([node.val for node in level])
-            level = [kid for n in level for kid in (n.left, n.right) if kid]
+            temp = []
+
+            for node in level:
+                temp.extend([node.left, node.right])
+
+            level = [leaf for leaf in temp if leaf]
 
         return ans
