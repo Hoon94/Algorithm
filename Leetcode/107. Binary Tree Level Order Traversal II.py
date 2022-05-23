0ae6aa333d111073dfefa79e321123c80a1ca647
Hoon94 <dleognsdl1@naver.com>
2022-05-23 19:00:26 +0900

    Binary Tree Level Order Traversal II

--- Leetcode/107. Binary Tree Level Order Traversal II.py
+++ Leetcode/107. Binary Tree Level Order Traversal II.py
@@ -1,4 +1,5 @@
 from typing import Optional, List
+import collections
 
 
 class TreeNode:
@@ -10,18 +11,17 @@ class TreeNode:
 
 class Solution:
     def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
-        stack = [(root, 0)]
-        res = []
+        queue, res = collections.deque([(root, 0)]), []
 
-        while stack:
-            node, level = stack.pop()
+        while queue:
+            node, level = queue.popleft()
 
             if node:
                 if len(res) < level + 1:
                     res.insert(0, [])
 
                 res[-(level + 1)].append(node.val)
-                stack.append((node.right, level + 1))
-                stack.append((node.left, level + 1))
+                queue.append((node.left, level + 1))
+                queue.append((node.right, level + 1))
 
         return res
