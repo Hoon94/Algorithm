2ae4a39b5ea3522c7798b37b3cbb706991e372e3
Hoon94 <dleognsdl1@naver.com>
2022-06-02 22:17:21 +0900

    Balanced Binary Tree

--- Leetcode/110. Balanced Binary Tree.py
+++ Leetcode/110. Balanced Binary Tree.py
@@ -10,16 +10,27 @@ class TreeNode:
 
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def check(root):
-            if root is None:
-                return 0
+        stack, node, last, depths = [], root, None, {}
 
-            left = check(root.left)
-            right = check(root.right)
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack[-1]
 
-            if left == -1 or right == -1 or abs(left - right) > 1:
-                return -1
+                if not node.right or last == node.right:
+                    node = stack.pop()
+                    left, right = depths.get(
+                        node.left, 0), depths.get(node.right, 0)
 
-            return 1 + max(left, right)
+                    if abs(left - right) > 1:
+                        return False
 
-        return check(root) != -1
+                    depths[node] = 1 + max(left, right)
+                    last = node
+                    node = None
+                else:
+                    node = node.right
+
+        return True
