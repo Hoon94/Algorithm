48781f383746f529cd0e8599d42acad5c972c544
Hoon94 <dleognsdl1@naver.com>
2022-06-09 19:31:09 +0900

    Path Sum

--- Leetcode/112. Path Sum.py
+++ Leetcode/112. Path Sum.py
@@ -13,18 +13,7 @@ class Solution:
         if not root:
             return False
 
-        queue = [(root, sum - root.val)]
+        if not root.left and not root.right and root.val == sum:
+            return True
 
-        while queue:
-            curr, val = queue.pop(0)
-
-            if not curr.left and not curr.right and val == 0:
-                return True
-
-            if curr.left:
-                queue.append((curr.left, val - curr.left.val))
-
-            if curr.right:
-                queue.append((curr.right, val - curr.right.val))
-
-        return False
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
