ca196b7afcb0e203f820b54c1e1e1350f6422c04
Hoon94 <dleognsdl1@naver.com>
2022-06-21 22:08:18 +0900

    Distinct Subsequences

--- Leetcode/115. Distinct Subsequences.py
+++ Leetcode/115. Distinct Subsequences.py
@@ -1,13 +1,13 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         l1, l2 = len(s) + 1, len(t) + 1
-        dp = [[1] * l2 for _ in range(l1)]
-
-        for j in range(1, l2):
-            dp[0][j] = 0
+        cur = [0] * l2
+        cur[0] = 1
 
         for i in range(1, l1):
-            for j in range(1, l2):
-                dp[i][j] = dp[i-1][j] + dp[i-1][j-1] * (s[i-1] == t[j-1])
+            pre = cur[:]
 
-        return dp[-1][-1]
+            for j in range(1, l2):
+                cur[j] = pre[j] + pre[j-1] * (s[i-1] == t[j-1])
+
+        return cur[-1]
