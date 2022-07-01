302ed245f428bb5de431f4459859e65f5ee9fff7
Hoon94 <dleognsdl1@naver.com>
2022-07-01 17:50:38 +0900

    Pascal's Triangle

--- Leetcode/118. Pascal's Triangle.py
+++ Leetcode/118. Pascal's Triangle.py
@@ -3,10 +3,15 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        ans = [[1] * i for i in range(1, numRows + 1)]
+        def helper(n):
+            if n:
+                helper(n - 1)
+                ans.append([1] * n)
 
-        for i in range(1, numRows):
-            for j in range(1, i):
-                ans[i][j] = ans[i-1][j] + ans[i-1][j-1]
+                for i in range(1, n - 1):
+                    ans[n-1][i] = ans[n-2][i] + ans[n-2][i-1]
+
+        ans = []
+        helper(numRows)
 
         return ans
