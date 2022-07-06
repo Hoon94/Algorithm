8945eb67e002ea6e58683c8cb7e81acf4c28b8bf
Hoon94 <dleognsdl1@naver.com>
2022-07-06 22:08:21 +0900

    Triangle

--- Leetcode/120. Triangle.py
+++ Leetcode/120. Triangle.py
@@ -6,14 +6,9 @@ class Solution:
         if not triangle:
             return
 
-        for i in range(1, len(triangle)):
+        for i in range(len(triangle) - 2, -1, -1):
             for j in range(len(triangle[i])):
-                if j == 0:
-                    triangle[i][j] += triangle[i - 1][j]
-                elif j == len(triangle[i]) - 1:
-                    triangle[i][j] += triangle[i - 1][j - 1]
-                else:
-                    triangle[i][j] += min(triangle[i - 1]
-                                          [j - 1], triangle[i - 1][j])
+                triangle[i][j] += min(triangle[i + 1][j],
+                                      triangle[i + 1][j + 1])
 
-        return min(triangle[-1])
+        return triangle[0][0]
