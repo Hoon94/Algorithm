8d391589d1214c79614fa1efd5e225c2716e006d
Hoon94 <dleognsdl1@naver.com>
2022-07-18 00:06:50 +0900

    Valid Palindrome

--- Leetcode/125. Valid Palindrome.py
+++ Leetcode/125. Valid Palindrome.py
@@ -2,4 +2,4 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         newS = [i.lower() for i in s if i.isalnum()]
 
-        return newS == newS[::-1]
+        return newS[:len(newS) / 2] == newS[(len(newS) + 1) / 2:][::-1]
