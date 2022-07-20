58ffebb6d248a259b5f44af6756262423da21961
Hoon94 <dleognsdl1@naver.com>
2022-07-20 20:49:28 +0900

    Word Ladder II

--- Leetcode/126. Word Ladder II.py
+++ Leetcode/126. Word Ladder II.py
@@ -11,21 +11,24 @@ class Solution:
         if endWord not in wordList:
             return []
 
-        found, q, nq = False, {beginWord}, set()
+        found, bq, eq, nq, rev = False, {beginWord}, {endWord}, set(), False
 
-        while q and not found:
-            words -= set(q)
-            for x in q:
+        while bq and not found:
+            words -= set(bq)
+            for x in bq:
                 for y in [x[:i] + c + x[i + 1:] for i in range(n) for c in string.ascii_lowercase]:
                     if y in words:
-                        if y == endWord:
+                        if y in eq:
                             found = True
                         else:
                             nq.add(y)
 
-                        tree[x].add(y)
+                        tree[y].add(x) if rev else tree[x].add(y)
 
-            q, nq = nq, set()
+            bq, nq = nq, set()
+
+            if len(bq) > len(eq):
+                bq, eq, rev = eq, bq, not rev
 
         def bt(x):
             return [[x]] if x == endWord else [[x] + rest for y in tree[x] for rest in bt(y)]
