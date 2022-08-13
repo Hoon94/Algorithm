eaf5c9d66398a632ed0707427533f1e8ec26d367
Hoon94 <dleognsdl1@naver.com>
2022-08-13 22:08:31 +0900

    Clone Graph

--- Leetcode/133. Clone Graph.py
+++ Leetcode/133. Clone Graph.py
@@ -1,6 +1,3 @@
-from collections import deque
-
-
 class Node:
     def __init__(self, val=0, neighbors=None):
         self.val = val
@@ -12,24 +9,23 @@ class Solution:
         if not node:
             return node
 
-        m, visited, stack = dict(), set(), deque([node])
-
-        while stack:
-            n = stack.pop()
-
-            if n in visited:
-                continue
-
-            visited.add(n)
-
-            if n not in m:
-                m[n] = Node(n.val)
-
-            for neigh in n.neighbors:
-                if neigh not in m:
-                    m[neigh] = Node(neigh.val)
-
-                m[n].neighbors.append(m[neigh])
-                stack.append(neigh)
+        m, visited = dict(), set()
+        self.dfs(node, m, visited)
 
         return m[node]
+
+    def dfs(self, n, m, visited):
+        if n in visited:
+            return
+
+        visited.add(n)
+
+        if n not in m:
+            m[n] = Node(n.val)
+
+        for neigh in n.neighbors:
+            if neigh not in m:
+                m[neigh] = Node(neigh.val)
+
+            m[n].neighbors.append(m[neigh])
+            self.dfs(neigh, m, visited)
