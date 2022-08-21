a5e2f32bf92415c9211ab1cc9d146cab208f0e96
Hoon94 <dleognsdl1@naver.com>
2022-08-21 21:52:43 +0900

    Single Number

--- Leetcode/136. Single Number.py
+++ Leetcode/136. Single Number.py
@@ -3,9 +3,4 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-
-        for num in nums:
-            res ^= num
-
-        return res
+        return 2 * sum(set(nums)) - sum(nums)
