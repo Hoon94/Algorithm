5970cdc912aa1b13d8ef49131b3010df745c3fe9
Hoon94 <dleognsdl1@naver.com>
2022-08-23 21:11:56 +0900

    Single Number

--- Leetcode/136. Single Number.py
+++ Leetcode/136. Single Number.py
@@ -1,7 +1,8 @@
 from typing import List
 from functools import reduce
+import operator
 
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return reduce(lambda x, y: x ^ y, nums)
+        return reduce(operator.xor, nums)
