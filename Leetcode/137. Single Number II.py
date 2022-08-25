cfc5e655e8ba64ef039ed6ea91773652690cf9e4
Hoon94 <dleognsdl1@naver.com>
2022-08-25 22:09:39 +0900

    Single Number II

--- Leetcode/137. Single Number II.py
+++ Leetcode/137. Single Number II.py
@@ -3,10 +3,18 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        one, two = 0, 0
+        ans = 0
+        is_neg = False
 
-        for x in nums:
-            one, two, three = one ^ x, two | (one & x), two & x
-            one, two = one & ~three, two & ~three
+        for i in range(32):
+            counter = 0
+            for num in nums:
+                if (num >> i) & 1:
+                    counter += 1
 
-        return one
+            if counter % 3 == 1:
+                ans += pow(2, i)
+                if i == 31:
+                    is_neg = True
+
+        return ans if not is_neg else ans - pow(2, 32)
