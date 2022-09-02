988f962e42dd308a13aaefccccd47a9e23fabff7
Hoon94 <dleognsdl1@naver.com>
2022-09-02 22:01:31 +0900

    Linked List Cycle

--- Leetcode/141. Linked List Cycle.py
+++ Leetcode/141. Linked List Cycle.py
@@ -9,17 +9,14 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head:
+        try:
+            slow = head
+            fast = head.next
+
+            while slow is not fast:
+                slow = slow.next
+                fast = fast.next.next
+
+            return True
+        except:
             return False
-
-        slow = head
-        fast = head.next
-
-        while slow != fast:
-            if not fast or not fast.next:
-                return False
-
-            slow = slow.next
-            fast = fast.next.next
-
-        return True
