bf8d0926fb0438c7fd081dea470115cf330c2d2d
Hoon94 <dleognsdl1@naver.com>
2022-09-27 23:29:38 +0900

    Find Minimum in Rotated Sorted Array

--- Leetcode/153. Find Minimum in Rotated Sorted Array.py
+++ Leetcode/153. Find Minimum in Rotated Sorted Array.py
@@ -4,5 +4,14 @@ import bisect
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        self.__getitem__ = lambda i: nums[i] <= nums[-1]
-        return nums[bisect.bisect(self, False, 0, len(nums))]
+        first, last = 0, len(nums) - 1
+
+        while first < last:
+            midpoint = (first + last) // 2
+
+            if nums[midpoint] > nums[last]:
+                first = midpoint + 1
+            else:
+                last = midpoint
+
+        return nums[first]
