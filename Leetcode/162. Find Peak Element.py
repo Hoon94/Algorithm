8841b79f76c66d149743304a26d72514af221ac2
Hoon94 <dleognsdl1@naver.com>
2022-10-04 23:33:12 +0900

    Find Peak Element

--- Leetcode/162. Find Peak Element.py
+++ Leetcode/162. Find Peak Element.py
@@ -3,14 +3,18 @@ from typing import List
 
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        low, high = 0, len(nums) - 1
+        left = 0
+        right = len(nums) - 1
 
-        while low < high:
-            mid = low + int((high - low) / 2)
+        while left < right - 1:
+            mid = (left + right) / 2
+
+            if nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]:
+                return mid
 
             if nums[mid] < nums[mid + 1]:
-                low = mid + 1
+                left = mid + 1
             else:
-                high = mid
+                right = mid - 1
 
-        return low
+        return left if nums[left] >= nums[right] else right
