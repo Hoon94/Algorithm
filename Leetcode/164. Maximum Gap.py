dd7ce96664dbe0ccb368ccd2b9f357ab76c7268f
Hoon94 <dleognsdl1@naver.com>
2022-10-06 22:59:23 +0900

    Maximum Gap

--- Leetcode/164. Maximum Gap.py
+++ Leetcode/164. Maximum Gap.py
@@ -3,27 +3,16 @@ from typing import List
 
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
-        if len(nums) < 2:
+        if len(nums) <= 1:
             return 0
 
-        hi, lo, ans = max(nums), min(nums), 0
-        bsize = (hi - lo) // (len(nums) - 1) or 1
-        buckets = [[] for _ in range(((hi - lo) // bsize) + 1)]
+        nums = [bin(num)[2:][::-1] for num in nums]
 
-        for n in nums:
-            buckets[(n - lo) // bsize].append(n)
+        for i in range(max(map(len, nums))):
+            nums0 = [x for x in nums if i >= len(x) or x[i] == '0']
+            nums1 = [x for x in nums if i < len(x) and x[i] == '1']
+            nums = nums0 + nums1
 
-        currhi = 0
+        nums = [int(num[::-1], 2) for num in nums]
 
-        for b in buckets:
-            if not len(b):
-                continue
-
-            prevhi, currlo = currhi or b[0], b[0]
-
-            for n in b:
-                currhi, currlo = max(currhi, n), min(currlo, n)
-
-            ans = max(ans, currlo - prevhi)
-
-        return ans
+        return max(nums[i] - nums[i - 1] for i in range(1, len(nums)))
