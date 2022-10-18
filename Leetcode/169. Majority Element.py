3749aa9ba3bb1853c87649347f111e876780b1d6
Hoon94 <dleognsdl1@naver.com>
2022-10-18 20:48:52 +0900

    Majority Element

--- Leetcode/169. Majority Element.py
+++ Leetcode/169. Majority Element.py
@@ -3,6 +3,4 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-
-        return nums[len(nums) // 2]
+        return sorted(nums)[len(nums) / 2]
