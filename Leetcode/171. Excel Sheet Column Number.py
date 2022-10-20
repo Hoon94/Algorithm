ef5ee020f186ddcfc70233328a205090752ad10f
Hoon94 <dleognsdl1@naver.com>
2022-10-20 20:49:36 +0900

    Excel Sheet Column Number

--- Leetcode/171. Excel Sheet Column Number.py
+++ Leetcode/171. Excel Sheet Column Number.py
@@ -2,7 +2,7 @@ class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         result = 0
 
-        for char, i in zip(columnTitle, range(len(columnTitle), 0, -1)):
-            result += 26 ** (i - 1) * (ord(char) - 64)
+        for c in columnTitle:
+            result = result * 26 + ord(c) - ord('A') + 1
 
         return result
