443e75a60265b3bb1d39cba0a06aef63497200b7
Hoon94 <dleognsdl1@naver.com>
2022-10-30 22:36:13 +0900

    Largest Number

--- Leetcode/179. Largest Number.py
+++ Leetcode/179. Largest Number.py
@@ -3,13 +3,16 @@ from typing import List
 
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        for i in range(len(nums), 0, -1):
-            tmp = 0
+        for i in range(len(nums)):
+            pos, cur = i, nums[i]
 
-            for j in range(i):
-                if not self.compare(nums[j], nums[tmp]):
-                    tmp = j
+            while pos > 0 and not self.compare(nums[pos - 1], cur):
+                nums[pos] = nums[pos - 1]
+                pos -= 1
 
-            nums[tmp], nums[i - 1] = nums[i - 1], nums[tmp]
+            nums[pos] = cur
 
         return str(int("".join(map(str, nums))))
+
+    def compare(self, n1, n2):
+        return str(n1) + str(n2) > str(n2) + str(n1)
