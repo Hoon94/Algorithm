bbc02480984bb79c7ff3df092c226539ff283491
Hoon94 <dleognsdl1@naver.com>
2022-10-31 20:27:08 +0900

    Largest Number

--- Leetcode/179. Largest Number.py
+++ Leetcode/179. Largest Number.py
@@ -3,16 +3,33 @@ from typing import List
 
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        for i in range(len(nums)):
-            pos, cur = i, nums[i]
-
-            while pos > 0 and not self.compare(nums[pos - 1], cur):
-                nums[pos] = nums[pos - 1]
-                pos -= 1
-
-            nums[pos] = cur
+        nums = self.mergeSort(nums, 0, len(nums) - 1)
 
         return str(int("".join(map(str, nums))))
 
-    def compare(self, n1, n2):
-        return str(n1) + str(n2) > str(n2) + str(n1)
+    def mergeSort(self, nums, l, r):
+        if l > r:
+            return
+
+        if l == r:
+            return [nums[l]]
+
+        mid = l + (r - l) // 2
+        left = self.mergeSort(nums, l, mid)
+        right = self.mergeSort(nums, mid + 1, r)
+
+        return self.merge(left, right)
+
+    def merge(self, l1, l2):
+        res, i, j = [], 0, 0
+        while i < len(l1) and j < len(l2):
+            if not self.compare(l1[i], l2[j]):
+                res.append(l2[j])
+                j += 1
+            else:
+                res.append(l1[i])
+                i += 1
+
+        res.extend(l1[i:] or l2[j:])
+
+        return res
