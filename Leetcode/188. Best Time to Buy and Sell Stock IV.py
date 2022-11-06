aad765a762a4e08800ae94a443c74ecc775400da
Hoon94 <dleognsdl1@naver.com>
2022-11-06 22:55:21 +0900

    Best Time to Buy and Sell Stock IV

--- Leetcode/188. Best Time to Buy and Sell Stock IV.py
+++ Leetcode/188. Best Time to Buy and Sell Stock IV.py
@@ -3,12 +3,14 @@ from typing import List
 
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        min_price = [float("inf")] * (k + 1)
-        max_profit = [0] * (k + 1)
+        if k == 0:
+            return 0
+
+        dp = [[1000, 0] for _ in range(k + 1)]
 
         for price in prices:
             for i in range(1, k + 1):
-                min_price[i] = min(min_price[i], price - max_profit[i - 1])
-                max_profit[i] = max(max_profit[i], price - min_price[i])
+                dp[i][0] = min(dp[i][0], price - dp[i - 1][1])
+                dp[i][1] = max(dp[i][1], price - dp[i][0])
 
-        return max_profit[k]
+        return dp[k][1]
