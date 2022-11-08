a72450620249e228267ce8c3e69be896b3a25693
Hoon94 <dleognsdl1@naver.com>
2022-11-08 21:40:07 +0900

    Rotate Array

--- Leetcode/189. Rotate Array.py
+++ Leetcode/189. Rotate Array.py
@@ -4,22 +4,22 @@ from typing import List
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         k = k % len(nums)
+        count = 0
+        start = 0
 
-        if k < 0:
-            k += len(nums)
+        while count < len(nums):
+            current = start
+            prev = nums[start]
 
-        self.reverse(nums, 0, len(nums) - k - 1)
-        self.reverse(nums, len(nums) - k, len(nums) - 1)
-        self.reverse(nums, 0, len(nums) - 1)
+            while True:
+                next = (current + k) % len(nums)
+                temp = nums[next]
+                nums[next] = prev
+                prev = temp
+                current = next
+                count += 1
 
-    def reverse(self, nums, i, j):
-        li = i
-        ri = j
+                if start == current:
+                    break
 
-        while li < ri:
-            temp = nums[li]
-            nums[li] = nums[ri]
-            nums[ri] = temp
-
-            li += 1
-            ri -= 1
+            start += 1
