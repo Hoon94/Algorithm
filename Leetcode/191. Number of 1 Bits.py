74ba0cd5a02bedf0789b9e99386fee0131ff3ad5
Hoon94 <dleognsdl1@naver.com>
2022-11-14 22:14:43 +0900

    Number of 1 Bits

--- Leetcode/191. Number of 1 Bits.py
+++ Leetcode/191. Number of 1 Bits.py
@@ -1,11 +1,7 @@
+import collections
+
+
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        count = 0
-
-        while n:
-            if n & 1:
-                count += 1
-
-            n = n >> 1
-
-        return count
+        counter = collections.Counter(bin(n)[2:])
+        return counter.get("1", 0)
