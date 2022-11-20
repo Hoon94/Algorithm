b5647d3fb35305c5ae1ba750b985785c8f553604
Hoon94 <dleognsdl1@naver.com>
2022-11-20 21:42:08 +0900

    Binary Tree Right Side View

--- Leetcode/199. Binary Tree Right Side View.py
+++ Leetcode/199. Binary Tree Right Side View.py
@@ -10,15 +10,14 @@ class TreeNode:
 
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        def collect(node, depth):
-            if node:
-                if depth == len(view):
-                    view.append(node.val)
-
-                collect(node.right, depth + 1)
-                collect(node.left, depth + 1)
-
         view = []
-        collect(root, 0)
+
+        if root:
+            level = [root]
+
+            while level:
+                view += level[-1].val,
+                level = [kid for node in level for kid in (
+                    node.left, node.right) if kid]
 
         return view
