220bec23be3991b35652b907c0a9656bb4afcf89
Hoon94 <dleognsdl1@naver.com>
2022-11-29 23:49:45 +0900

    Isomorphic Strings

--- Leetcode/205. Isomorphic Strings.py
+++ Leetcode/205. Isomorphic Strings.py
@@ -1,11 +1,11 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        d1, d2 = {}, {}
+        d1, d2 = [[] for _ in range(256)], [[] for _ in range(256)]
 
         for i, val in enumerate(s):
-            d1[val] = d1.get(val, []) + [i]
+            d1[ord(val)].append(i)
 
         for i, val in enumerate(t):
-            d2[val] = d2.get(val, []) + [i]
+            d2[ord(val)].append(i)
 
-        return sorted(d1.values()) == sorted(d2.values())
+        return sorted(d1) == sorted(d2)
