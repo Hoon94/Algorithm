78b29f05fd4ecbd6b79129ccac4ccc842275926f
Hoon94 <dleognsdl1@naver.com>
2022-12-01 21:14:15 +0900

    Isomorphic Strings

--- Leetcode/205. Isomorphic Strings.py
+++ Leetcode/205. Isomorphic Strings.py
@@ -1,3 +1,3 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return len(set(zip(s, t))) == len(set(s)) == len(set(t))
+        return [s.find(i) for i in s] == [t.find(j) for j in t]
