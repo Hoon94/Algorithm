ead54464629951ae7d01af80dd6fc71f80002044
Hoon94 <dleognsdl1@naver.com>
2022-12-11 23:33:09 +0900

    Implement Trie (Prefix Tree)

---
+++ Leetcode/208. Implement Trie (Prefix Tree).py
@@ -0,0 +1,34 @@
+class Trie:
+
+    def __init__(self):
+        self.trie = {}
+
+    def insert(self, word: str) -> None:
+        t = self.trie
+
+        for c in word:
+            if c not in t:
+                t[c] = {}
+            t = t[c]
+
+        t["-"] = True
+
+    def search(self, word: str) -> bool:
+        t = self.trie
+
+        for c in word:
+            if c not in t:
+                return False
+            t = t[c]
+
+        return "-" in t
+
+    def startsWith(self, prefix: str) -> bool:
+        t = self.trie
+
+        for c in prefix:
+            if c not in t:
+                return False
+            t = t[c]
+
+        return True
