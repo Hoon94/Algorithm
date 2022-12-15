fe09dd23f44a54300a09cc602a048929fac3283e
Hoon94 <dleognsdl1@naver.com>
2022-12-15 22:10:39 +0900

    Word Search II

--- Leetcode/212. Word Search II.py
+++ Leetcode/212. Word Search II.py
@@ -5,7 +5,9 @@ from collections import defaultdict
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        def Trie(): return defaultdict(Trie)
+        def Trie():
+            return defaultdict(Trie)
+
         trie = Trie()
         END = True
 
@@ -17,23 +19,28 @@ class Solution:
         def findstr(i, j, t):
             if END in t:
                 res.add(t[END])
-                # return
+
             letter = board[i][j]
             board[i][j] = ""
-            if i > 0 and board[i-1][j] in t:
-                findstr(i-1, j, t[board[i-1][j]])
-            if j > 0 and board[i][j-1] in t:
-                findstr(i, j-1, t[board[i][j-1]])
-            if i < len(board)-1 and board[i+1][j] in t:
-                findstr(i+1, j, t[board[i+1][j]])
-            if j < len(board[0])-1 and board[i][j+1] in t:
-                findstr(i, j+1, t[board[i][j+1]])
-            board[i][j] = letter
 
+            if i > 0 and board[i - 1][j] in t:
+                findstr(i - 1, j, t[board[i - 1][j]])
+
+            if j > 0 and board[i][j - 1] in t:
+                findstr(i, j - 1, t[board[i][j - 1]])
+
+            if i < len(board) - 1 and board[i + 1][j] in t:
+                findstr(i + 1, j, t[board[i + 1][j]])
+
+            if j < len(board[0]) - 1 and board[i][j + 1] in t:
+                findstr(i, j + 1, t[board[i][j + 1]])
+
+            board[i][j] = letter
             return
 
         for i, row in enumerate(board):
             for j, char in enumerate(row):
                 if board[i][j] in trie:
                     findstr(i, j, trie[board[i][j]])
+
         return res
