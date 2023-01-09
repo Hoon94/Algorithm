b7f13d7bdaacceb4f17754139d743ad301d39145
Hoon94 <dleognsdl1@naver.com>
2023-01-09 23:26:05 +0900

    Maximal Square

--- Leetcode/221. Maximal Square.py
+++ Leetcode/221. Maximal Square.py
@@ -3,11 +3,14 @@ from typing import List
 
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        for i, r in enumerate(matrix):
-            r = matrix[i] = map(int, r)
-            for j, c in enumerate(r):
-                if i * j * c:
-                    r[j] = min(matrix[i - 1][j], r[j - 1],
-                               matrix[i - 1][j - 1]) + 1
+        area = 0
+        if matrix:
+            p = [0] * len(matrix[0])
+            for row in matrix:
+                s = map(int, row)
+                for j, c in enumerate(s[1:], 1):
+                    s[j] *= min(p[j - 1], p[j], s[j - 1]) + 1
+                area = max(area, max(s) ** 2)
+                p = s
 
-        return max(map(max, matrix + [[0]])) ** 2
+        return area
