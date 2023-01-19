4532fd39aae8f472f4778143f28cd56385b5ab4e
Hoon94 <dleognsdl1@naver.com>
2023-01-19 23:33:53 +0900

    Implement Stack using Queues

--- Leetcode/225. Implement Stack using Queues.py
+++ Leetcode/225. Implement Stack using Queues.py
@@ -4,21 +4,26 @@ from collections import deque
 class MyStack:
 
     def __init__(self):
-        self.q = deque()
+        self.q1 = deque()
+        self.q2 = deque()
+        self._top = None
 
     def push(self, x: int) -> None:
-        self.q.append(x)
-        q_len = len(self.q)
-
-        while q_len > 1:
-            self.q.append(self.q.popleft())
-            q_len -= 1
+        self.q1.append(x)
+        self._top = x
 
     def pop(self) -> int:
-        return self.q.popleft()
+        while len(self.q1) > 1:
+            self._top = self.q1.popleft()
+            self.q2.append(self._top)
+
+        result = self.q1.popleft()
+        self.q1, self.q2 = self.q2, self.q1
+
+        return result
 
     def top(self) -> int:
-        return self.q[0]
+        return self._top
 
     def empty(self) -> bool:
-        return len(self.q) == 0
+        return len(self.q1) == 0
