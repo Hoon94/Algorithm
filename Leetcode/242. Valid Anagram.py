40274fa019eac5ceafef8dffd4feac135218263d
Hoon94 <dleognsdl1@naver.com>
2023-02-25 23:48:15 +0900

    Valid Anagram

--- Leetcode/242. Valid Anagram.py
+++ Leetcode/242. Valid Anagram.py
@@ -1,11 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dic1, dic2 = {}, {}
+        dic1, dic2 = [0] * 26, [0] * 26
 
         for item in s:
-            dic1[item] = dic1.get(item, 0) + 1
+            dic1[ord(item) - ord('a')] += 1
 
         for item in t:
-            dic2[item] = dic2.get(item, 0) + 1
+            dic2[ord(item) - ord('a')] += 1
 
         return dic1 == dic2
