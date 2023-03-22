4d80b08002dadd6ef54edd40fbf2d2af8edff749
Hoon94 <dleognsdl1@naver.com>
2023-03-22 23:47:46 +0900

    Add Digits

--- Leetcode/258. Add Digits.py
+++ Leetcode/258. Add Digits.py
@@ -1,9 +1,12 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num == 0:
-            return 0
+        while num > 9:
+            sum = 0
 
-        if num % 9 == 0:
-            return 9
-        else:
-            return (num % 9)
+            while num:
+                sum += num % 10
+                num = num // 10
+
+            num = sum
+
+        return num
