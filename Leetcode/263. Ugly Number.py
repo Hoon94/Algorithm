6c1c987c13319306b7438d47cd01878c73bbaad3
Hoon94 <dleognsdl1@naver.com>
2023-03-25 03:28:11 +0900

    Ugly Number II

--- Leetcode/263. Ugly Number.py
+++ Leetcode/263. Ugly Number.py
@@ -1,8 +1,22 @@
 class Solution:
-    def isUgly(self, n: int) -> bool:
-        for p in 2, 3, 5:
+    def nthUglyNumber(self, n: int) -> int:
+        ugly = [1]
+        i2, i3, i5 = 0, 0, 0
 
-            while n % p == 0 < n:
-                n /= p
+        while n > 1:
+            u2, u3, u5 = 2 * ugly[i2], 3 * ugly[i3], 5 * ugly[i5]
+            umin = min((u2, u3, u5))
 
-        return n == 1
+            if umin == u2:
+                i2 += 1
+
+            if umin == u3:
+                i3 += 1
+
+            if umin == u5:
+                i5 += 1
+
+            ugly.append(umin)
+            n -= 1
+
+        return ugly[-1]
