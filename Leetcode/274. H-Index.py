c04fc63534ef3d0eac67fc7ba82597f01b415813
Hoon94 <dleognsdl1@naver.com>
2023-03-31 23:38:50 +0900

    H-Index

--- Leetcode/274. H-Index.py
+++ Leetcode/274. H-Index.py
@@ -3,10 +3,4 @@ from typing import List
 
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort(reverse=True)
-
-        for idx, citation in enumerate(citations):
-            if idx >= citation:
-                return idx
-
-        return len(citations)
+        return sum(i < j for i, j in enumerate(sorted(citations, reverse=True)))
