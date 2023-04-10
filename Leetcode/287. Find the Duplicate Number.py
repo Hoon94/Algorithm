7cde478144d06e6eb762506e4471c615fdff7ded
Hoon94 <dleognsdl1@naver.com>
2023-04-10 23:43:08 +0900

    Find the Duplicate Number

--- Leetcode/287. Find the Duplicate Number.py
+++ Leetcode/287. Find the Duplicate Number.py
@@ -3,9 +3,25 @@ from typing import List
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        N = len(nums)
+        N = len(nums) - 1
+        lo, hi = 1, N
 
-        for i, num in enumerate(nums):
-            for j in range(i + 1, N):
-                if nums[j] == num:
-                    return num
+        while lo < hi:
+            mi = (lo + hi) >> 1
+            less, equal = 0, 0
+
+            for num in nums:
+                if num < mi:
+                    less += 1
+                elif num == mi:
+                    equal += 1
+
+            if equal > 1:
+                return mi
+
+            if less >= mi:
+                hi = mi - 1
+            else:
+                lo = mi + 1
+
+        return lo
