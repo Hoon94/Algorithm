7a983904592a2bab9ab0738ea9e362cb3dd7045f
Hoon94 <dleognsdl1@naver.com>
2023-04-11 21:28:58 +0900

    Find the Duplicate Number

--- Leetcode/287. Find the Duplicate Number.py
+++ Leetcode/287. Find the Duplicate Number.py
@@ -4,24 +4,15 @@ from typing import List
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         N = len(nums) - 1
-        lo, hi = 1, N
+        nbits = N.bit_length()
+        ans = 0
 
-        while lo < hi:
-            mi = (lo + hi) >> 1
-            less, equal = 0, 0
+        for p in range(nbits):
+            mask = 1 << p
+            a = sum(1 if num & mask else 0 for num in nums)
+            b = sum(1 if num & mask else 0 for num in range(1, N + 1))
 
-            for num in nums:
-                if num < mi:
-                    less += 1
-                elif num == mi:
-                    equal += 1
+            if a > b:
+                ans |= mask
 
-            if equal > 1:
-                return mi
-
-            if less >= mi:
-                hi = mi - 1
-            else:
-                lo = mi + 1
-
-        return lo
+        return ans
