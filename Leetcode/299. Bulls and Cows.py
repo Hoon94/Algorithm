c79122d94e2c2ec2645834c0305bdf5349ad5608
Hoon94 <dleognsdl1@naver.com>
2023-04-18 23:37:37 +0900

    Bulls and Cows

--- Leetcode/299. Bulls and Cows.py
+++ Leetcode/299. Bulls and Cows.py
@@ -1,11 +1,19 @@
+from collections import Counter
+
+
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        bull = 0
-        for i in range(len(secret)):
-            bull += int(secret[i] == guess[i])
+        lookup = Counter(secret)
+        x, y = 0, 0
 
-        cows = 0
-        for c in set(secret):
-            cows += min(secret.count(c), guess.count(c))
+        for i in range(len(guess)):
+            if secret[i] == guess[i]:
+                x += 1
+                lookup[secret[i]] -= 1
 
-        return f"{bull}A{cows-bull}B"
+        for i in range(len(guess)):
+            if guess[i] in lookup and secret[i] != guess[i] and lookup[guess[i]] > 0:
+                y += 1
+                lookup[guess[i]] -= 1
+
+        return "{}A{}B".format(x, y)
