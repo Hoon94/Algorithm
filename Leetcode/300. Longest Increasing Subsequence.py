612c7c779bcb977e4fab2ddc2c129a50eef0a961
Hoon94 <dleognsdl1@naver.com>
2023-04-25 23:33:25 +0900

    Longest Increasing Subsequence

--- Leetcode/300. Longest Increasing Subsequence.py
+++ Leetcode/300. Longest Increasing Subsequence.py
@@ -4,12 +4,12 @@ from bisect import bisect_left
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        total_number = len(nums)
-        dp = [0 for _ in range(total_number)]
+        arr = [nums.pop(0)]
 
-        for i in range(1, total_number):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[i], dp[j] + 1)
+        for n in nums:
+            if n > arr[-1]:
+                arr.append(n)
+            else:
+                arr[bisect_left(arr, n)] = n
 
-        return max(dp) + 1
+        return len(arr)
