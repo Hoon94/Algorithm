eaaf6f3190f2dd2e782ce888681af204856b625a
Hoon94 <dleognsdl1@naver.com>
2023-04-28 23:51:33 +0900

    Bulls and Cows

--- Leetcode/299. Bulls and Cows.py
+++ Leetcode/299. Bulls and Cows.py
@@ -3,21 +3,25 @@ from collections import defaultdict
 
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        diff_dict = defaultdict(int)
-        num_bulls, num_cows = 0, 0
+        secret_dict = {}
+        guess_dict = {}
+        bulls = 0
+        cows = 0
 
-        for char1, char2 in zip(secret, guess):
-            if char1 == char2:
-                num_bulls += 1
+        for i in range(len(secret)):
+            if secret[i] == guess[i]:
+                bulls += 1
             else:
-                if diff_dict[char2] < 0:
-                    num_cows += 1
+                if secret[i] in guess_dict and guess_dict[secret[i]] > 0:
+                    cows += 1
+                    guess_dict[secret[i]] -= 1
+                else:
+                    secret_dict[secret[i]] = secret_dict.get(secret[i], 0) + 1
 
-                diff_dict[char2] += 1
+                if guess[i] in secret_dict and secret_dict[guess[i]] > 0:
+                    cows += 1
+                    secret_dict[guess[i]] -= 1
+                else:
+                    guess_dict[guess[i]] = guess_dict.get(guess[i], 0) + 1
 
-                if diff_dict[char1] > 0:
-                    num_cows += 1
-
-                diff_dict[char1] -= 1
-
-        return str(num_bulls) + 'A' + str(num_cows) + 'B'
+        return str(bulls) + "A" + str(cows) + "B"
