6f6969214bbb6bacd165313b6d8c0feb1caa8963
Hoon94 <dleognsdl1@naver.com>
2023-05-08 23:38:47 +0900

    Range Sum Query - Mutable

---
+++ Leetcode/307. Range Sum Query - Mutable.py
@@ -0,0 +1,49 @@
+from typing import List
+
+
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.n = len(nums)
+        self.tree = [0] * (2 * self.n)
+        self.buildTree(nums)
+
+    def update(self, index: int, val: int) -> None:
+        index += self.n
+        self.tree[index] = val
+
+        while index > 0:
+            left = right = index
+
+            if index % 2 == 0:
+                right = index + 1
+            else:
+                left = index - 1
+
+            self.tree[index // 2] = self.tree[left] + self.tree[right]
+            index //= 2
+
+    def sumRange(self, left: int, right: int) -> int:
+        left += self.n
+        right += self.n
+        res = 0
+
+        while left <= right:
+            if left % 2 == 1:
+                res += self.tree[left]
+                left += 1
+
+            if right % 2 == 0:
+                res += self.tree[right]
+                right -= 1
+
+            left //= 2
+            right //= 2
+
+        return res
+
+    def buildTree(self, nums: List[int]) -> None:
+        for i in range(self.n, 2 * self.n):
+            self.tree[i] = nums[i - self.n]
+
+        for i in range(self.n - 1, 0, -1):
+            self.tree[i] = self.tree[2 * i] + self.tree[2 * i + 1]
